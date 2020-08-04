4979c8c9decf2bf2ceb2ab3249535b6d3489f04f
haojh <haojh@amazon.com>
2020-08-04 12:19:25 -0700

    fix: Add procstat rule to config migration
    Add "user" to tagexclude

---
+++ cfg/migrate/procstat.go
@@ -0,0 +1,28 @@
+package migrate
+
+func init() {
+	AddRule(ProcStatRule)
+}
+
+//  [inputs]
+//  [[inputs.procstat]]
+//    fieldpass = ["cpu_usage", "memory_rss"]
+//    pid_file = "/var/run/example1.pid"
+//    pid_finder = "native"
+//+   tagexclude = ["user"]
+func ProcStatRule(conf map[string]interface{}) error {
+	ps := getItem(conf, "inputs", "procstat")
+
+	for _, p := range ps {
+		tagexclude, ok := p["tagexclude"].([]interface{})
+		var newTagEx []interface{}
+		if ok {
+			newTagEx = append(newTagEx, tagexclude...)
+		}
+		newTagEx = append(newTagEx, "user")
+
+		p["tagexclude"] = newTagEx
+	}
+
+	return nil
+}
