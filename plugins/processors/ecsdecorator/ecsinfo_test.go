9e1417fcde8e412d92f2d2d450f974eeffe5216b
Dejun Hu <hudejun@amazon.com>
2020-09-24 09:04:54 -0700

    Use correct cgroup path for task with new ARN format

---
+++ plugins/processors/ecsdecorator/ecsinfo_test.go
@@ -0,0 +1,19 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package ecsdecorator
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTaskCgroupPathFromARN(t *testing.T) {
+	oldFormatARN := "arn:aws:ecs:region:aws_account_id:task/task-id"
+	newFormatARN := "arn:aws:ecs:region:aws_account_id:task/cluster-name/task-id"
+	result, _ := getTaskCgroupPathFromARN(oldFormatARN)
+	assert.Equal(t, "task-id", result, "Expected to be equal")
+	result, _ = getTaskCgroupPathFromARN(newFormatARN)
+	assert.Equal(t, "task-id", result, "Expected to be equal")
+}
