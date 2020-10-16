e6bb9ebb6c1c2ab3da9c22e126e25d58e14dfdab
Ping Xiang <pinxiang@amazon.com>
2020-10-16 19:06:08 +0000

    Update license header for code adapted from Prometheus

--- plugins/inputs/prometheus_scraper/start.go
+++ plugins/inputs/prometheus_scraper/start.go
@@ -1,6 +1,22 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: MIT
 
+// The following code is based on Prometheus project https://github.com/prometheus/prometheus/blob/master/cmd/prometheus/main.go
+// and we did modification to remove the logic related to flag handling, Rule manager, TSDB, Web handler, and Notifier.
+
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package prometheus_scraper
 
 import (
