e27ce6b97419f2a533d10411e4ff04aff7e69a02
Dejun Hu <hudejun@amazon.com>
2020-10-26 10:34:29 -0700

    Make allowed_pending_messages to be configurable for statsd

---
+++ translator/translate/metrics/metrics_collect/statsd/ruleAllowedPendingMessages.go
@@ -0,0 +1,27 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package statsd
+
+import (
+	"github.com/aws/amazon-cloudwatch-agent/translator"
+)
+
+type AllowedPendingMessages struct {
+}
+
+const SectionKey_AllowedPendingMessages = "allowed_pending_messages"
+
+func (obj *AllowedPendingMessages) ApplyRule(input interface{}) (returnKey string, returnVal interface{}) {
+	returnKey, returnVal = translator.DefaultCase(SectionKey_AllowedPendingMessages, "", input)
+	if returnVal != "" {
+		// By default json unmarshal will store number as float64
+		return returnKey, int(returnVal.(float64))
+	}
+	return "", nil
+}
+
+func init() {
+	obj := new(AllowedPendingMessages)
+	RegisterRule(SectionKey_AllowedPendingMessages, obj)
+}
