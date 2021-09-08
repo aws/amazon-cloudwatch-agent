a9968890c720e9556936151b7e1acf8a4bf5b439
Yoshyuki Kanno <kannoyos@amazon.com>
2021-09-08 09:29:46 +0000

    fix wrong variadic parameters when request dropped

--- plugins/outputs/cloudwatchlogs/pusher.go
+++ plugins/outputs/cloudwatchlogs/pusher.go
@@ -286,7 +286,7 @@ func (p *pusher) send() {
 
 		wait := retryWait(retryCount)
 		if time.Since(startTime)+wait > p.RetryDuration {
-			p.Log.Errorf("All %v retries to %v/%v failed for PutLogEvents, request dropped.", p.Group, p.Stream, retryCount)
+			p.Log.Errorf("All %v retries to %v/%v failed for PutLogEvents, request dropped.", retryCount, p.Group, p.Stream)
 		}
 
 		p.Log.Warnf("Retried %v time, going to sleep %v before retrying.", retryCount, wait)
