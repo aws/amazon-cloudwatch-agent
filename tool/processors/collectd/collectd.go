8876de53079cee781826a0bcd0f1733f11a2c9ea
GlennChia <glennchia7@gmail.com>
2021-10-09 11:25:09 +0800

    chore: concise CollectD install warning message
    Co-authored-by: Adam Mateen <adamym@amazon.com>

--- tool/processors/collectd/collectd.go
+++ tool/processors/collectd/collectd.go
@@ -21,7 +21,7 @@ func (p *processor) Process(ctx *runtime.Context, config *data.Config) {
 	if ctx.OsParameter == util.OsTypeWindows {
 		return
 	}
-	yes := util.Yes("Do you want to monitor metrics from CollectD? Note that depending on the system, some require the CollectD software to be installed on your server (e.g. Amazon Linux 2) or the /usr/share/collectd/types.db file to be created.")
+	yes := util.Yes("Do you want to monitor metrics from CollectD? WARNING: CollectD must be installed or the Agent will fail to start")
 	if yes {
 		collection := config.MetricsConf().Collection()
 		collection.CollectD = new(collectd.CollectD)
