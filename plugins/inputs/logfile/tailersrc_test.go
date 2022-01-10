724902fb4adf33777a246e3344fb99fdb1e927a9
Andrew Huynh <andhuy@amazon.com>
2022-01-10 16:54:47 -0500

    tune sleep duration for github

--- plugins/inputs/logfile/tailersrc_test.go
+++ plugins/inputs/logfile/tailersrc_test.go
@@ -495,7 +495,7 @@ func publishLogsToFile(file *os.File, matchedLog, unmatchedLog string, n, multiL
 	var sleepDuration time.Duration
 	if multiLineWaitMs > 0 {
 		multilineWaitPeriod = time.Duration(multiLineWaitMs) * time.Millisecond
-		sleepDuration = time.Duration(multiLineWaitMs * 2) * time.Millisecond
+		sleepDuration = time.Duration(multiLineWaitMs * 4) * time.Millisecond
 	}
 
 	for i := 0; i < n; i++ {
