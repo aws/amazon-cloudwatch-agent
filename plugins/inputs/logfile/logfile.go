8f7bc1bbcd80f6a768f43e28f0e4a53e01a3da8a
Ateeqi <ateeqi@amazon.com>
2022-01-18 10:12:50 -0700

    check retentionSettings in 1 loop

--- plugins/inputs/logfile/logfile.go
+++ plugins/inputs/logfile/logfile.go
@@ -407,15 +407,16 @@ func (t *LogFile) checkForDuplicateRetentionSettings() {
 			logGroup := strings.ToLower(fileconfig.LogGroupName)
 			configMap[logGroup] += 1
 		}
-
-	}
-	for i := range t.FileConfig {
-		fileconfig := &t.FileConfig[i]
-		// log group has Retention settings in multiple places: throw an error
 		if fileconfig.LogGroupName != "" && configMap[strings.ToLower(fileconfig.LogGroupName)] > 1 {
 			panic(fmt.Sprintf("error: retention for the same log group set in multiple places. Log Group Name: %v", fileconfig.LogGroupName))
 		}
+
 	}
+	//for i := range t.FileConfig {
+	//	fileconfig := &t.FileConfig[i]
+	//	// log group has Retention settings in multiple places: throw an error
+	//
+	//}
 }
 
 // Compressed file should be skipped.
