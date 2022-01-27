7c911b2cd22f65ba666d71f44889f239b9e1b19c
Nguyen <khanhntd@88665a0eaf54.ant.amazon.com>
2022-01-27 15:33:47 -0500

    add comments for test case

--- plugins/processors/k8sdecorator/stores/utils_test.go
+++ plugins/processors/k8sdecorator/stores/utils_test.go
@@ -16,8 +16,10 @@ func TestUtils_parseDeploymentFromReplicaSet(t *testing.T) {
 }
 
 func TestUtils_parseCronJobFromJob(t *testing.T) {
+	//For CronJobControllV2 which is after K8s v1.21
 	assert.Equal(t, parseCronJobFromJob("hello-"+strconv.FormatInt(time.Now().Unix()/60, 10)), "hello")
 	assert.Equal(t, parseCronJobFromJob("hello-"+strconv.FormatInt(time.Now().Unix()/60, 10)+"abc"), "")
+	//For CronJob which is before K8s v1.21
 	assert.Equal(t, parseCronJobFromJob("hello-"+strconv.FormatInt(time.Now().Unix()/60, 10)), "hello")
 	assert.Equal(t, parseCronJobFromJob("hello-"+strconv.FormatInt(time.Now().Unix()/60, 10)+"-name"), "")
 }
