8a250049e127007717ecb201950e132767d602fa
Seth L <81644108+sethAmazon@users.noreply.github.com>
2022-03-24 09:37:10 -0400

    Add Terraform For Windows Test (#391)

---
+++ integration/terraform/ec2/win/main.tf
@@ -0,0 +1,37 @@
+resource "aws_instance" "integration-test" {
+  ami                    = data.aws_ami.latest.id
+  instance_type          = var.ec2_instance_type
+  key_name               = var.key_name
+  iam_instance_profile   = var.iam_instance_profile
+  vpc_security_group_ids = var.vpc_security_group_ids
+  get_password_data = true
+  provisioner "remote-exec" {
+    # @TODO when @ZhenyuTan-amz adds windows tests add "make integration-test"
+    inline = [
+      "echo clone and install agent",
+      "git clone ${var.github_repo}",
+      "cd amazon-cloudwatch-agent",
+      "git reset --hard ${var.github_sha}",
+      "aws s3 cp s3://cloudwatch-agent-integration-bucket/integration-test/packaging/${var.github_sha}/amazon-cloudwatch-agent.msi .",
+      "msiexec /i amazon-cloudwatch-agent.msi",
+    ]
+    connection {
+      type     = "ssh"
+      user     = "Administrator"
+      private_key = var.ssh_key
+      password = rsadecrypt(self.password_data, var.ssh_key)
+      host     = self.public_dns
+      target_platform = "windows"
+    }
+  }
+}
+
+data "aws_ami" "latest" {
+  most_recent = true
+  owners      = ["self"]
+
+  filter {
+    name   = "name"
+    values = [var.ami]
+  }
+}
---
+++ integration/terraform/ec2/win/variables.tf
@@ -0,0 +1,44 @@
+variable "ec2_instance_type" {
+  type = string
+  default = "t3a.xlarge"
+}
+
+variable "key_name" {
+  type = string
+  default = "cwagent-integ-test-key"
+}
+
+variable "iam_instance_profile" {
+  type = string
+  default = "CloudWatchAgentServerRole"
+}
+
+variable "vpc_security_group_ids" {
+  type = list(string)
+  default = ["sg-013585129c1f92bf0"]
+}
+
+variable "region" {
+  type = string
+  default = "us-west-2"
+}
+
+variable "ami" {
+  type = string
+  default = ""
+}
+
+variable "ssh_key" {
+  type = string
+  default = ""
+}
+
+variable "github_sha" {
+  type = string
+  default = ""
+}
+
+variable "github_repo" {
+  type = string
+  default = ""
+}
