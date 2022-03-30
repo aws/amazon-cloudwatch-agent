d0bb2676b4fd5e25d17d74d94c24123f9a563b7b
Ateeqi <ateeqi@amazon.com>
2022-03-29 22:56:31 -0400

    fix test

--- translator/translate/logs/logs_collected/files/collect_list/collect_list.go
+++ translator/translate/logs/logs_collected/files/collect_list/collect_list.go
@@ -91,24 +91,26 @@ func checkForConflictingRetentionSettings(logConfigs []interface{}) []interface{
 	configMap := make(map[string]int)
 	for _, logConfig := range logConfigs {
 		if logConfigMap, ok := logConfig.(map[string]interface{}); ok {
-			logGroup := strings.ToLower(logConfigMap[LogGroupNameSectionKey].(string))
-			// if retention is 0, -1 or less it's either invalid or default
-			retention := logConfigMap["retention_in_days"].(int)
-			if retention < 1 {
-				continue
-			}
-			// if the configMap[logGroup] exists, retention has been set for the same logGroup somewhere
-			if configMap[logGroup] != 0 {
-				// different retentions has been set for the same log group, panic and stop the agent
-				if configMap[logGroup] != retention {
-					translator.AddErrorMessages(
-						GetCurPath()+SectionKey,
-						fmt.Sprintf("Different Retention values can't be set for the same log group: %v", logGroup))
+			// skip if retention is not set
+			if retention, ok := logConfigMap["retention_in_days"].(int); ok {
+				logGroup := strings.ToLower(logConfigMap[LogGroupNameSectionKey].(string))
+				// if retention is 0, -1 or less it's either invalid or default
+				if retention < 1 {
+					continue
 				}
-				// The same retention for a log group has been configured in multiple places. Unset it so that the retention api is only called once
-				logConfigMap["retention_in_days"] = -1
-			} else {
-				configMap[logGroup] = retention
+				// if the configMap[logGroup] exists, retention has been set for the same logGroup somewhere
+				if configMap[logGroup] != 0 {
+					// different retentions has been set for the same log group, panic and stop the agent
+					if configMap[logGroup] != retention {
+						translator.AddErrorMessages(
+							GetCurPath()+SectionKey,
+							fmt.Sprintf("Different Retention values can't be set for the same log group: %v", logGroup))
+					}
+					// The same retention for a log group has been configured in multiple places. Unset it so that the retention api is only called once
+					logConfigMap["retention_in_days"] = -1
+				} else {
+					configMap[logGroup] = retention
+				}
 			}
 		}
 	}
