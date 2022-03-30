f935c07858f71f12ef6b47d9276213b15b6c452e
Ateeqi <ateeqi@amazon.com>
2022-03-30 11:44:45 -0400

    check for null during log group name conversion

--- translator/translate/logs/logs_collected/files/collect_list/collect_list.go
+++ translator/translate/logs/logs_collected/files/collect_list/collect_list.go
@@ -6,17 +6,16 @@ package collect_list
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"path/filepath"
-	"sort"
-	"strings"
-
 	"github.com/aws/amazon-cloudwatch-agent/translator"
 	"github.com/aws/amazon-cloudwatch-agent/translator/context"
 	"github.com/aws/amazon-cloudwatch-agent/translator/jsonconfig/mergeJsonRule"
 	"github.com/aws/amazon-cloudwatch-agent/translator/jsonconfig/mergeJsonUtil"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/agent"
 	parent "github.com/aws/amazon-cloudwatch-agent/translator/translate/logs/logs_collected/files"
+	"io/ioutil"
+	"path/filepath"
+	"sort"
+	"strings"
 )
 
 type Rule translator.Rule
@@ -93,23 +92,25 @@ func checkForConflictingRetentionSettings(logConfigs []interface{}) []interface{
 		if logConfigMap, ok := logConfig.(map[string]interface{}); ok {
 			// skip if retention is not set
 			if retention, ok := logConfigMap["retention_in_days"].(int); ok {
-				logGroup := strings.ToLower(logConfigMap[LogGroupNameSectionKey].(string))
-				// if retention is 0, -1 or less it's either invalid or default
-				if retention < 1 {
-					continue
-				}
-				// if the configMap[logGroup] exists, retention has been set for the same logGroup somewhere
-				if configMap[logGroup] != 0 {
-					// different retentions has been set for the same log group, panic and stop the agent
-					if configMap[logGroup] != retention {
-						translator.AddErrorMessages(
-							GetCurPath()+SectionKey,
-							fmt.Sprintf("Different Retention values can't be set for the same log group: %v", logGroup))
+				if logGroup, ok := logConfigMap[LogGroupNameSectionKey].(string); ok {
+					// if retention is 0, -1 or less it's either invalid or default
+					logGroup = strings.ToLower(logGroup)
+					if retention < 1 {
+						continue
 					}
-					// The same retention for a log group has been configured in multiple places. Unset it so that the retention api is only called once
-					logConfigMap["retention_in_days"] = -1
-				} else {
-					configMap[logGroup] = retention
+					// if the configMap[logGroup] exists, retention has been set for the same logGroup somewhere
+					if configMap[logGroup] != 0 {
+						// different retentions has been set for the same log group, panic and stop the agent
+						if configMap[logGroup] != retention {
+							translator.AddErrorMessages(
+								GetCurPath(),
+								fmt.Sprintf("Different Retention values can't be set for the same log group: %v", logGroup))
+						}
+						// The same retention for a log group has been configured in multiple places. Unset it so that the retention api is only called once
+						logConfigMap["retention_in_days"] = -1
+					} else {
+						configMap[logGroup] = retention
+					}
 				}
 			}
 		}
