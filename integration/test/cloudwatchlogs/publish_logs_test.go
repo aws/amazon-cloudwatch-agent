53a223ce19053d3259eb021a2630609d53e1996f
Andrew Huynh <andhuy@amazon.com>
2022-04-11 19:21:56 -0400

    updated test to write to log file natively in Go

--- integration/test/cloudwatchlogs/publish_logs_test.go
+++ integration/test/cloudwatchlogs/publish_logs_test.go
@@ -8,11 +8,13 @@ package cloudwatchlogs
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/amazon-cloudwatch-agent/integration/test"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 	"log"
-	"strconv"
+	"os"
+	"sync"
 
 	"testing"
 	"time"
@@ -20,9 +22,13 @@ import (
 
 const (
 	configOutputPath = "/opt/aws/amazon-cloudwatch-agent/bin/config.json"
-	logScriptPath    = "resources/write_logs.sh"
-	agentRunTime     = 1 * time.Minute
+	logLineId1       = "foo"
+	logLineId2       = "bar"
+	logFilePath      = "/tmp/test.log"  // TODO: not sure how well this will work on Windows
+	agentRuntime     = 10 * time.Second // default flush interval is 5 seconds
 )
+
+var logLineIds = []string{logLineId1, logLineId2}
 
 type input struct {
 	testName        string
@@ -55,6 +61,8 @@ func TestWriteLogsToCloudWatch(t *testing.T) {
 		// fail fast so we don't continue the test
 		t.Fatalf("Error occurred while creating SDK config: %v", err)
 	}
+
+	// TODO: this only works for EC2 based testing
 	client := imds.NewFromConfig(c)
 	metadata, err := client.GetInstanceIdentityDocument(ctx, &imds.GetInstanceIdentityDocumentInput{})
 	if err != nil {
@@ -67,12 +75,17 @@ func TestWriteLogsToCloudWatch(t *testing.T) {
 	for _, param := range testParameters {
 		t.Run(param.testName, func(t *testing.T) {
 			start := time.Now()
+			var wg sync.WaitGroup
+
 			test.CopyFile(param.configPath, configOutputPath)
 
-			// give some buffer time before writing to ensure consistent
-			// usage of the StartTime parameter for GetLogEvents
-			time.Sleep(5 * time.Second)
-			writeLogsAndRunAgent(param.iterations, agentRunTime)
+			test.StartAgent(configOutputPath)
+
+			wg.Add(1)
+			go writeLogs(t, &wg, logFilePath, param.iterations)
+			wg.Wait()
+			time.Sleep(agentRuntime)
+			test.StopAgent()
 
 			// check CWL to ensure we got the expected number of logs in the log stream
 			test.ValidateLogs(t, instanceId, instanceId, param.numExpectedLogs, start)
@@ -80,15 +93,26 @@ func TestWriteLogsToCloudWatch(t *testing.T) {
 	}
 }
 
-func writeLogsAndRunAgent(iterations int, runtime time.Duration) {
-	log.Printf("Writing %d of each log type\n", iterations)
-	test.RunShellScript(logScriptPath, strconv.Itoa(iterations)) // write logs before starting the agent
-	log.Println("Finished writing logs. Sleeping before starting agent...")
-	time.Sleep(10 * time.Second)
+func writeLogs(t *testing.T, wg *sync.WaitGroup, filePath string, iterations int) {
+	defer wg.Done() // always release the lock
 
-	test.StartAgent(configOutputPath)
-	time.Sleep(runtime)
-	test.StopAgent()
+	f, err := os.Create(filePath)
+	if err != nil {
+		t.Fatalf("Error occurred creating log file for writing: %v", err)
+	}
+	defer f.Close()
+
+	for i := 0; i < iterations; i++ {
+		ts := time.Now()
+		for _, id := range logLineIds {
+			_, err = f.WriteString(fmt.Sprintf("%s - %s This is a log line.\n", ts.Format(time.RFC3339), id))
+			if err != nil {
+				// don't need to fatal error here. if a log line doesn't get written, the count
+				// when validating the log stream should be incorrect and fail there.
+				t.Logf("Error occurred writing log line: %v", err)
+			}
+		}
+	}
 }
 
 func cleanUp(instanceId string) {
