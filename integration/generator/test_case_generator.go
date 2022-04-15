6f87fadd089c497a924559870832fa81fb88b307
Seth L <81644108+sethAmazon@users.noreply.github.com>
2022-04-15 10:21:43 -0400

    Generate JSON File With Linux Test AMI X Tag (#440)

---
+++ integration/generator/test_case_generator.go
@@ -0,0 +1,75 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+const (
+	linux   = "linux"
+	windows = "windows"
+	mac     = "mac"
+	tag     = "tag"
+)
+
+//you can't have a const map in golang
+var osToTagMap = map[string][]string{
+	linux:   {"integration"},
+	windows: {},
+	mac:     {},
+}
+
+func main() {
+	for osType, tags := range osToTagMap {
+		testMatrix := genMatrix(osType, tags)
+		writeTestMatrixFile(osType, testMatrix)
+	}
+}
+
+func genMatrix(targetOS string, tags []string) []map[string]string {
+	openTestMatrix, err := os.Open(fmt.Sprintf("integration/generator/resources/%v_test_matrix.json", targetOS))
+	if err != nil {
+		panic(fmt.Sprintf("can't read file %v_test_matrix.json err %v", targetOS, err))
+	}
+	byteValueTestMatrix, _ := ioutil.ReadAll(openTestMatrix)
+	_ = openTestMatrix.Close()
+	var testMatrix []map[string]string
+	err = json.Unmarshal([]byte(byteValueTestMatrix), &testMatrix)
+	if err != nil {
+		panic(fmt.Sprintf("can't unmarshall file %v_test_matrix.json err %v", targetOS, err))
+	}
+
+	var testMatrixComplete []map[string]string
+	for _, test := range testMatrix {
+		testLine := copyMap(test)
+		for _, testTag := range tags {
+			testLine[tag] = testTag
+		}
+		testMatrixComplete = append(testMatrixComplete, testLine)
+	}
+	return testMatrixComplete
+}
+
+func writeTestMatrixFile(targetOS string, testMatrix []map[string]string) {
+	bytes, err := json.MarshalIndent(testMatrix, "", " ")
+	if err != nil {
+		panic(fmt.Sprintf("Can't marshal json for target os %v, err %v", targetOS, err))
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("integration/generator/resources/%v_complete_test_matrix.json", targetOS), bytes, os.ModePerm)
+	if err != nil {
+		panic(fmt.Sprintf("Can't write json to file for target os %v, err %v", targetOS, err))
+	}
+}
+
+func copyMap(mapToCopy map[string]string) map[string]string {
+	testLine := make(map[string]string)
+	for key, value := range mapToCopy {
+		testLine[key] = value
+	}
+	return testLine
+}
