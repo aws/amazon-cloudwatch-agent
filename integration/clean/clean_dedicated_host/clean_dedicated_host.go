5f02e8ca998f3a00b18ab705054436b79b0ee11d
Seth Levine <sethlevi@amazon.com>
2022-04-19 12:04:35 -0400

    Print Out Dedicated Host State In Clean Action

--- integration/clean/clean_dedicated_host/clean_dedicated_host.go
+++ integration/clean/clean_dedicated_host/clean_dedicated_host.go
@@ -46,8 +46,8 @@ func cleanDedicatedHost() error {
 
 	dedicatedHostIds := make([]string, 0)
 	for _, dedicatedHost := range dedicatedHosts {
-		log.Printf("dedicated host id %v experation date %v dedicated host creation date raw %v",
-			*dedicatedHost.HostId, expirationDateDedicatedHost, *dedicatedHost.AllocationTime)
+		log.Printf("dedicated host id %v experation date %v dedicated host creation date raw %v host state %v",
+			*dedicatedHost.HostId, expirationDateDedicatedHost, *dedicatedHost.AllocationTime, dedicatedHost.State)
 		if expirationDateDedicatedHost.After(*dedicatedHost.AllocationTime) && dedicatedHost.State == types.AllocationStateAvailable {
 			log.Printf("Try to delete dedicated host %s tags %v launch-date %s", *dedicatedHost.HostId, dedicatedHost.Tags, *dedicatedHost.AllocationTime)
 			dedicatedHostIds = append(dedicatedHostIds, *dedicatedHost.HostId)
