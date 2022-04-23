50c4774d738dec40a0121dba3a81bc4a9ba31195
Khanh Nguyen <khanhntd@amazon.com>
2022-04-22 20:52:37 -0400

    Add back mode EC2

--- translator/config/envconst.go
+++ translator/config/envconst.go
@@ -6,6 +6,8 @@ package config
 const (
 	RUN_IN_CONTAINER        = "RUN_IN_CONTAINER"
 	RUN_IN_CONTAINER_TRUE   = "True"
+	RUN_IN_AWS              = "RUN_IN_AWS"
+	RUN_IN_AWS_TRUE         = "True"
 	USE_DEFAULT_CONFIG      = "USE_DEFAULT_CONFIG"
 	USE_DEFAULT_CONFIG_TRUE = "True"
 	HOST_NAME               = "HOST_NAME"
