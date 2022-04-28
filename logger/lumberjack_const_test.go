a6c28d6e95195f969d8a6bef860ff91f4e2a0617
ZhenyuTan-amz <tazhenyu@amazon.com>
2022-04-28 00:50:17 -0400

    Lumberjack logger fix

--- logger/lumberjack_const_test.go
+++ logger/lumberjack_const_test.go
@@ -4,20 +4,13 @@
 package logger
 
 import (
-	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/influxdata/telegraf/logger"
-	"github.com/influxdata/wlog"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 func TestWriteLogToFile(t *testing.T) {
@@ -103,56 +96,11 @@ func TestWriteToTruncatedFile(t *testing.T) {
 	assert.Equal(t, f[19:], []byte("Z I! SHOULD BE FIRST\n"))
 }
 
-func TestWriteToFileInRotation(t *testing.T) {
-	tempDir, err := ioutil.TempDir("", "LogRotation")
-	require.NoError(t, err)
-	config := createBasicLogConfig(filepath.Join(tempDir, "test.log"))
-	config.LogTarget = logger.LogTargetFile
-	wlog.SetLevel(wlog.INFO)
-	maxsize := 1
-	lumberjackLogger := &lumberjack.Logger{
-		Filename:   config.Logfile,
-		MaxSize:    maxsize,
-		MaxBackups: 5,
-		MaxAge:     1,
-		Compress:   false,
-	}
-	w, _ := logger.NewTelegrafWriter(lumberjackLogger, config)
-	log.SetOutput(w)
-	var logWriter interface{} = lumberjackLogger
-	// Close the writer here, otherwise the temp folder cannot be deleted because the current log file is in use.
-	closer, isCloser := logWriter.(io.Closer)
-	assert.True(t, isCloser)
-	defer func() { closer.Close(); os.RemoveAll(tempDir) }()
-
-	s := "I! TEST 2 " + strings.Repeat("a", maxsize*1024*1024-100)
-	log.Printf(s) // Writes 1M bytes, will rotate
-
-	files, _ := ioutil.ReadDir(tempDir)
-	assert.Equal(t, 1, len(files))
-
-	// make sure the length is less than maxsize M, otherwise logger will discard the line.
-	log.Printf(s) // Writes 1M bytes, will rotate
-
-	files, _ = ioutil.ReadDir(tempDir)
-	assert.Equal(t, 2, len(files))
-
-	log.Printf(s) // Writes 1M bytes, will rotate
-	log.Printf(s) // Writes 1M bytes, will rotate
-
-	files, _ = ioutil.ReadDir(tempDir)
-	for _, file := range files {
-		fmt.Printf("%v/%v, size:%v\n", tempDir, file.Name(), file.Size())
-	}
-
-	assert.Equal(t, 4, len(files))
-
-}
-
 func createBasicLogConfig(filename string) logger.LogConfig {
 	return logger.LogConfig{
 		Logfile:             filename,
-		LogTarget:           logger.LogTargetFile,
+		LogTarget:           LogTargetLumberjack,
 		RotationMaxArchives: -1,
+		LogWithTimezone: 	 "UTC",
 	}
 }
