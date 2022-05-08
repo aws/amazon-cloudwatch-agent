38a3f9c88c5e29d7297e2f90a368932c1b8fc810
Khanh Nguyen <khanhntd@amazon.com>
2022-05-07 21:28:46 -0400

    Fix some generate release note path and add version for release document

--- Tools/release/generate-release-note.sh
+++ Tools/release/generate-release-note.sh
@@ -7,8 +7,8 @@ if [[ -z "${VERSION}" ]]; then
         error_exit "Missing input for flag version"
 fi
 
-sed "s/__VERSION__/$VERSION/g" tools/release/header.md.template >header
-sed "s/__VERSION__/$VERSION/g" tools/release/downloading-links.md.template >downloading-links
+sed "s/__VERSION__/$VERSION/g" Tools/release/header.md.template >header
+sed "s/__VERSION__/$VERSION/g" Tools/release/downloading-links.md.template >downloading-links
 # Formats changelog sections: **<label>:** becomes ### <label>
 # Skips the first 4 lines (title and version number)
 sed -e 1,4d -e "s/^\*\*\(.*\)\:\*\*/### \1/g" "docs/releases/${VERSION}.md" >changelog
