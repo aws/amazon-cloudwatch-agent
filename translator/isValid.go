e162e31bf8beca511a09b19a09dd1b0f89da3b9e
Ameen <ateeqi@amazon.com>
2022-05-24 11:19:38 -0400

    Support setting retention to 6, 7, 8, 9 years (#469)
    * Support setting retention to 6 years
    
    * add support for 7, 8, and 9 years to match logs team
    
    * add coment for validRetentionInDays
    
    * change doc to link to cloudwatch agent docs

--- translator/isValid.go
+++ translator/isValid.go
@@ -11,7 +11,9 @@ import (
 //ErrorMessages will provide detail error messages to user
 var ErrorMessages = []string{}
 var InfoMessages = []string{}
-var ValidRetentionInDays = []string{"-1", "1", "3", "5", "7", "14", "30", "60", "90", "120", "150", "180", "365", "400", "545", "731", "1827", "3653"}
+
+// ValidRetentionInDays is based on what's supported by PutRetentionPolicy. See https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Agent-Configuration-File-Details.html#CloudWatch-Agent-Configuration-File-Logssection.
+var ValidRetentionInDays = []string{"-1", "1", "3", "5", "7", "14", "30", "60", "90", "120", "150", "180", "365", "400", "545", "731", "1827", "2192", "2557", "2922", "3288", "3653"}
 
 //IsValid checks whether the mandatory config parameter is valid
 func IsValid(input interface{}, key string, path string) bool {
