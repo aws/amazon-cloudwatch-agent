b5228eddb567af9213e14459f2dbca477a1b02b1
Andrew Huynh <andhuy@amazon.com>
2022-07-19 12:00:13 -0400

    remove log entirely

--- translator/util/ec2util/ec2util.go
+++ translator/util/ec2util/ec2util.go
@@ -6,7 +6,6 @@ package ec2util
 import (
 	"errors"
 	"fmt"
-	"log"
 	"net"
 	"sync"
 	"time"
@@ -103,7 +102,7 @@ func (e *ec2Util) deriveEC2MetadataFromIMDS() error {
 	if hostname, err := md.GetMetadata("hostname"); err == nil {
 		e.Hostname = hostname
 	} else {
-		log.Println("E! [EC2] Fetch hostname from EC2 metadata fail:", err)
+		fmt.Println("E! [EC2] Fetch hostname from EC2 metadata fail:", err)
 	}
 
 	// More information on API: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
@@ -113,7 +112,7 @@ func (e *ec2Util) deriveEC2MetadataFromIMDS() error {
 		e.PrivateIP = instanceIdentityDocument.PrivateIP
 		e.InstanceID = instanceIdentityDocument.InstanceID
 	} else {
-		log.Println("E! [EC2] Fetch identity document from EC2 metadata fail:", err)
+		fmt.Println("E! [EC2] Fetch identity document from EC2 metadata fail:", err)
 	}
 
 	return nil
