249447d25fdf36e72e6b48d302c738e503ff914d
Okan Kocabalkanli <okanko@amazon.com>
2022-07-25 22:34:22 +0000

    t

--- integration/generator/test_case_generator.go
+++ integration/generator/test_case_generator.go
@@ -133,7 +133,7 @@ func genMatrixForReleases(targetOS string, testDirList []string) []map[string]st
 
 func getReleases(startDate int ,EndDate int ) []string{
 	rawData := os.Getenv("SHA_DATA")
-	fmt.Prinln(rawData)
+	fmt.Println(rawData)
 	tagData := strings.Split(string(rawData),"\n")
 	var tagList []string
 	fmt.Println(tagData)
