0b4cda57f8a6746f1e7232aa47bb8114924000ff
Okan Kocabalkanli <okanko@amazon.com>
2022-07-27 13:47:43 +0000

    working release

--- getOldMetrics.sh
+++ getOldMetrics.sh
@@ -5,6 +5,6 @@ echo $startDate $endDate
 go run  --tags=generator integration/generator/test_case_generator.go  $startDate $endDate
 
 
-git add ec2_performance_old_test_matrix.json
+git add integration/generator/resources/ec2_performance_old_test_matrix.json
 git commit -m "Testing releases from $startDate to $endDate"
 git push
