969ecb374e29b08761c814845da93b59125b5841
Okan Kocabalkanli <okanko@amazon.com>
2022-08-03 18:48:24 +0000

    updated shell

--- getOldMetrics.sh
+++ getOldMetrics.sh
@@ -1,10 +1,10 @@
 export OLD="true"
-startDate=${1:-1627262978}
-endDate=${2:-1658798978}
-echo $startDate $endDate
+startDate=${1:-1622398650}
+endDate=${2:-1653934651}
 go run  --tags=generator integration/generator/test_case_generator.go  $startDate $endDate
-
-
+cleanStart=$(date -d @$startDate +%Y/%m/%d)
+cleanEnd=$(date -d @$endDate +%Y/%m/%d)
+echo "$cleanStart to $cleanEnd"
 git add integration/generator/resources/ec2_performance_old_test_matrix.json
-git commit -m "Testing releases from $startDate to $endDate"
+git commit -m "Testing releases from $cleanStart to $cleanEnd"
 git push
