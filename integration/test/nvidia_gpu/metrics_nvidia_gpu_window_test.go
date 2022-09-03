97d8e467dc8ecea480df419a130ebacefa9f11b0
Khanh Nguyen <khanhntd@amazon.com>
2022-09-03 13:18:21 -0400

    Finish NVIDIA GPU--amend

--- integration/test/nvidia_gpu/metrics_nvidia_gpu_window_test.go
+++ integration/test/nvidia_gpu/metrics_nvidia_gpu_window_test.go
@@ -17,12 +17,12 @@ const (
 	configWindowsJSON               = "resources/config_windows.json"
 	metricWindowsnamespace          = "NvidiaGPUWindowsTest"
 	configWindowsOutputPath         = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\config.json"
-	agentWindowsLogPath             = "C:\\ProgramData\\\\Amazon\\AmazonCloudWatchAgent\\Logs\\amazon-cloudwatch-agent.log"
-	agentWindowsRuntime             = 2 * time.Minute
+	agentWindowsLogPath             = "C:\\ProgramData\\Amazon\\AmazonCloudWatchAgent\\Logs\\amazon-cloudwatch-agent.log"
+	agentWindowsRuntime             = 3 * time.Minute
 	numberofWindowsAppendDimensions = 1
 )
 
-var expectedNvidiaGPUWindowsMetrics = []string{"Memory % Committed Bytes In Use", "nvidia_smi utilization_gpu", "nvidia_smi utilization_memory", "nvidia_smi power_draw", "nvidia_smitemperature_gpu"}
+var expectedNvidiaGPUWindowsMetrics = []string{"Memory % Committed Bytes In Use", "nvidia_smi utilization_gpu", "nvidia_smi utilization_memory", "nvidia_smi power_draw", "nvidia_smi temperature_gpu"}
 
 func TestNvidiaGPUWindows(t *testing.T) {
 	t.Run("Run CloudWatchAgent with Nvidia-smi on Windows", func(t *testing.T) {
