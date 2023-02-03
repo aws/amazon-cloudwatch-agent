d0aae10b6f023a691240cbabd8d0f2a4f4ee1494
Khanh Nguyen <khanhntd@amazon.com>
2023-02-02 21:21:15 -0500

    Updates

---
+++ precache-manifest.333cbf869a9a35138f539c3a0b2bfd94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "691b0bcf1dda46c51673",
+    "url": "/amazon-cloudwatch-agent/static/css/main.6d10b9cd.chunk.css"
+  },
+  {
+    "revision": "691b0bcf1dda46c51673",
+    "url": "/amazon-cloudwatch-agent/static/js/main.691b0bcf.chunk.js"
+  },
+  {
+    "revision": "24ba8badcf940109971f",
+    "url": "/amazon-cloudwatch-agent/static/js/1.24ba8bad.chunk.js"
+  },
+  {
+    "revision": "d62744cf6da44dddc7bf",
+    "url": "/amazon-cloudwatch-agent/static/js/2.d62744cf.chunk.js"
+  },
+  {
+    "revision": "fa9762c81df64b2c1f4a",
+    "url": "/amazon-cloudwatch-agent/static/js/runtime~main.fa9762c8.js"
+  },
+  {
+    "revision": "c6e6d8b7eb4faa460acd0c441058d21a",
+    "url": "/amazon-cloudwatch-agent/index.html"
+  }
+];
