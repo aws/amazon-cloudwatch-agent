c7bddd9cb28c3deac9f33f964b4864a75c999b83
Billy Zhou <billyzh@amazon.com>
2023-03-13 11:04:31 -0400

    build_upload_binary.sh uses latest commit hash

--- test/integration/build_upload_binary.sh
+++ test/integration/build_upload_binary.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
-# builds the binary and uploads to s3, using the config.json
+# builds the agent from the current commit hash and uploads to s3
 
 configPath="./config.json"
+cwaGithubSha=$( git rev-parse HEAD )
 s3Bucket=$(cat $configPath | python3 -c "import sys, json; print(json.load(sys.stdin)['s3Bucket'])")
-cwaGithubSha=$(cat $configPath | python3 -c "import sys, json; print(json.load(sys.stdin)['cwaGithubSha'])")
+s3Url="s3://$s3Bucket/integration-test/binary/$cwaGithubSha"
 
 echo "s3Bucket=$s3Bucket"
 echo "cwaGithubSha=$cwaGithubSha"
+echo "s3Url=$s3Url"
 
-cd ..
+cd ../../
 echo "Building agent"
-make build package-rpm package-deb package-win
+make release
 echo "Uploading binary to S3"
-aws s3 cp ./build/bin "s3://$s3Bucket/integration-test/binary/$cwaGithubSha" --recursive
+aws s3 cp ./build/bin $s3Url --recursive
 
