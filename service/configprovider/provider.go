24592ba4cfd5014971fe2d6fa46fcb8fce5fa283
Jeffrey Chien <chienjef@amazon.com>
2023-08-09 18:57:30 -0400

    Add expandconverter to config provider. (#398)
    (cherry picked from commit 09dfdf828489916219822e4ff48660f70f1fd4fe)

---
+++ service/configprovider/provider.go
@@ -0,0 +1,25 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package configprovider
+
+import (
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/confmap/converter/expandconverter"
+	"go.opentelemetry.io/collector/confmap/provider/fileprovider"
+	"go.opentelemetry.io/collector/otelcol"
+)
+
+func Get(configPath string) (otelcol.ConfigProvider, error) {
+	fprovider := fileprovider.New()
+	settings := otelcol.ConfigProviderSettings{
+		ResolverSettings: confmap.ResolverSettings{
+			URIs:       []string{configPath},
+			Converters: []confmap.Converter{expandconverter.New()},
+			Providers: map[string]confmap.Provider{
+				fprovider.Scheme(): fprovider,
+			},
+		},
+	}
+	return otelcol.NewConfigProvider(settings)
+}
