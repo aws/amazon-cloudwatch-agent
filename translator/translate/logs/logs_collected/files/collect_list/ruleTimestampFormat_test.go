cccfe1b9c0bcef459ce09ef0e240ef41d5b780ad
Yared Taye <vtayyare@amazon.com>
2023-09-28 22:22:46 -0400

    add default for %-m and timestamp translation unit tests

---
+++ translator/translate/logs/logs_collected/files/collect_list/ruleTimestampFormat_test.go
@@ -0,0 +1,106 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package collect_list
+
+import (
+	"encoding/json"
+	//"github.com/aws/amazon-cloudwatch-agent/translator"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestApplyTimestampFormatRule(t *testing.T) {
+	r := new(TimestampRegax)
+	var input interface{}
+	e := json.Unmarshal([]byte(`{
+			"timestamp_format": "%b %d %H:%M:%S"
+	}`), &input)
+	if e == nil {
+		actualReturnKey, retVal := r.ApplyRule(input)
+		assert.Equal(t, "timestamp_regex", actualReturnKey)
+		assert.NotNil(t, retVal)
+	} else {
+		panic(e)
+	}
+}
+
+func TestApplyInvalidTimestampFormatRule(t *testing.T) {
+	regex := new(TimestampRegax)
+	var input interface{}
+	e := json.Unmarshal([]byte(`{
+			"timestamp_format": "foo"
+	}`), &input)
+	if e == nil {
+		actualReturnKey, retVal := regex.ApplyRule(input)
+		assert.Equal(t, "timestamp_regex", actualReturnKey)
+		assert.Equal(t, "(foo)", retVal)
+	} else {
+		panic(e)
+	}
+}
+
+func TestApplyTimestampFormatLayoutRule(t *testing.T) {
+	layout := new(TimestampLayout)
+	var input interface{}
+	e := json.Unmarshal([]byte(`{
+			"timestamp_format": "%b %d %H:%M:%S"
+	}`), &input)
+	if e == nil {
+		actualReturnKey, retVal := layout.ApplyRule(input)
+		assert.Equal(t, "timestamp_layout", actualReturnKey)
+		assert.NotNil(t, retVal)
+		assert.Equal(t, "Jan 02 15:04:05", retVal)
+
+	} else {
+		panic(e)
+	}
+}
+
+func TestApplyInvalidTimestampFormatLayoutRule(t *testing.T) {
+	layout := new(TimestampLayout)
+	var input interface{}
+	e := json.Unmarshal([]byte(`{
+			"timezone": "UTC"
+	}`), &input)
+	if e == nil {
+		actualReturnKey, retVal := layout.ApplyRule(input)
+		assert.Equal(t, "", actualReturnKey)
+		assert.Equal(t, "", retVal)
+	} else {
+		panic(e)
+	}
+}
+
+func TestApplyTimestampFormatZeroPaddingRule(t *testing.T) {
+	// compare zero padding and not zero padding options are
+	// translating as the same regex for %d/%-d and %m/%-m
+	nonZeroRegax := new(TimestampRegax)
+	zeroRegax := new(TimestampRegax)
+
+	var non_zero interface{}
+	var zero interface{}
+
+	e := json.Unmarshal([]byte(`{
+			"timestamp": "%m %-d %H:%M:%S"
+	}`), &non_zero)
+
+	f := json.Unmarshal([]byte(`{
+			"timestamp": "%m %d %H:%M:%S"
+	}`), &zero)
+
+	if (e == nil) || (f == nil) {
+		zeroActualReturnKey, zeroRetVal := zeroRegax.ApplyRule(zero)
+		nonZeroActualReturnKey, nonZeroRetVal := nonZeroRegax.ApplyRule(non_zero)
+		assert.Equal(t, "timestamp_regex", zeroActualReturnKey)
+		assert.Equal(t, zeroActualReturnKey, nonZeroActualReturnKey)
+		assert.NotNil(t, zeroRetVal)
+		assert.NotNil(t, nonZeroRetVal)
+		assert.Equal(t, "(\\s{0,1}\\d{1,2} \\s{0,1}\\d{1,2} \\d{2}:\\d{2}:\\d{2})", zeroRetVal)
+		assert.Equal(t, zeroRetVal, nonZeroRetVal)
+
+	} else {
+		panic(e)
+	}
+}
