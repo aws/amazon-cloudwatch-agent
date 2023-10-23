4228271433d881adaf500230da1496415cc84757
Dinakar Chappa <dchappa@amazon.com>
2023-10-23 13:54:17 -0400

    raises num lines to 100k

--- plugins/inputs/logfile/logfile_test.go
+++ plugins/inputs/logfile/logfile_test.go
@@ -434,7 +434,7 @@ func createWriteRead(t *testing.T, prefix string, logFile *LogFile, done chan bo
 	defer close(evts)
 	// Choose a large enough number of lines so that even high-spec hosts will not
 	// complete receiving logEvents before the 2nd createWriteRead() goroutine begins.
-	const numLines int = 10000
+	const numLines int = 100000
 	const msg string = "this is the best log line ever written to a file"
 	writeLines(t, file, numLines, msg)
 	file.Close()
