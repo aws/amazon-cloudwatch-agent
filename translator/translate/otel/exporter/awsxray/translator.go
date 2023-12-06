a81dc83af486de2fda1b45a0856894cc2a702b27
Jeffrey Chien <chienjef@amazon.com>
2023-12-06 18:46:38 -0500

    Fix X-Ray exporter to respect CA bundle.
    Fix CloudWatch Logs exporter to respect region.

--- translator/translate/otel/exporter/awsxray/translator.go
+++ translator/translate/otel/exporter/awsxray/translator.go
@@ -6,12 +6,14 @@ package awsxray
 import (
 	_ "embed"
 	"fmt"
+	"os"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/exporter/awsxrayexporter"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap"
 	"go.opentelemetry.io/collector/exporter"
 
+	"github.com/aws/amazon-cloudwatch-agent/cfg/envconfig"
 	"github.com/aws/amazon-cloudwatch-agent/internal/retryer"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/agent"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
@@ -68,33 +70,34 @@ func (t *translator) Translate(conf *confmap.Conf) (component.Config, error) {
 	if err := c.Unmarshal(&cfg); err != nil {
 		return nil, fmt.Errorf("unable to unmarshal into awsxrayexporter config: %w", err)
 	}
-	cfg.RoleARN = getRoleARN(conf)
-	cfg.Region = getRegion(conf)
+	cfg.AWSSessionSettings.CertificateFilePath = os.Getenv(envconfig.AWS_CA_BUNDLE)
+	if endpointOverride, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, common.EndpointOverrideKey)); ok {
+		cfg.AWSSessionSettings.Endpoint = endpointOverride
+	}
+	cfg.AWSSessionSettings.IMDSRetries = retryer.GetDefaultRetryNumber()
+	if localMode, ok := common.GetBool(conf, common.ConfigKey(common.TracesKey, common.LocalModeKey)); ok {
+		cfg.AWSSessionSettings.LocalMode = localMode
+	}
+	if insecure, ok := common.GetBool(conf, common.ConfigKey(common.TracesKey, common.InsecureKey)); ok {
+		cfg.AWSSessionSettings.NoVerifySSL = insecure
+	}
+	if concurrency, ok := common.GetNumber(conf, common.ConfigKey(common.TracesKey, concurrencyKey)); ok {
+		cfg.AWSSessionSettings.NumberOfWorkers = int(concurrency)
+	}
 	if profileKey, ok := agent.Global_Config.Credentials[agent.Profile_Key]; ok {
 		cfg.AWSSessionSettings.Profile = fmt.Sprintf("%v", profileKey)
 	}
+	if proxyAddress, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, common.ProxyOverrideKey)); ok {
+		cfg.AWSSessionSettings.ProxyAddress = proxyAddress
+	}
+	if resourceARN, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, resourceARNKey)); ok {
+		cfg.AWSSessionSettings.ResourceARN = resourceARN
+	}
+	cfg.AWSSessionSettings.Region = getRegion(conf)
+	cfg.AWSSessionSettings.RoleARN = getRoleARN(conf)
 	if credentialsFileKey, ok := agent.Global_Config.Credentials[agent.CredentialsFile_Key]; ok {
 		cfg.AWSSessionSettings.SharedCredentialsFile = []string{fmt.Sprintf("%v", credentialsFileKey)}
 	}
-	if endpointOverride, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, common.EndpointOverrideKey)); ok {
-		cfg.Endpoint = endpointOverride
-	}
-	if concurrency, ok := common.GetNumber(conf, common.ConfigKey(common.TracesKey, concurrencyKey)); ok {
-		cfg.NumberOfWorkers = int(concurrency)
-	}
-	if resourceARN, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, resourceARNKey)); ok {
-		cfg.ResourceARN = resourceARN
-	}
-	if insecure, ok := common.GetBool(conf, common.ConfigKey(common.TracesKey, common.InsecureKey)); ok {
-		cfg.NoVerifySSL = insecure
-	}
-	if localMode, ok := common.GetBool(conf, common.ConfigKey(common.TracesKey, common.LocalModeKey)); ok {
-		cfg.LocalMode = localMode
-	}
-	if proxyAddress, ok := common.GetString(conf, common.ConfigKey(common.TracesKey, common.ProxyOverrideKey)); ok {
-		cfg.ProxyAddress = proxyAddress
-	}
-	cfg.AWSSessionSettings.IMDSRetries = retryer.GetDefaultRetryNumber()
 	cfg.MiddlewareID = &agenthealth.TracesID
 	return cfg, nil
 }
--- translator/translate/otel/exporter/otel_aws_cloudwatch_logs/translator_test.go
+++ translator/translate/otel/exporter/otel_aws_cloudwatch_logs/translator_test.go
@@ -11,32 +11,44 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/confmap"
 
+	"github.com/aws/amazon-cloudwatch-agent/cfg/envconfig"
 	legacytranslator "github.com/aws/amazon-cloudwatch-agent/translator"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/agent"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
 )
 
 func TestTranslator(t *testing.T) {
+	t.Setenv(envconfig.AWS_CA_BUNDLE, "/ca/bundle")
+	agent.Global_Config.Region = "us-east-1"
+	agent.Global_Config.Role_arn = "global_arn"
 	tt := NewTranslatorWithName(common.PipelineNameEmfLogs)
 	require.EqualValues(t, "awscloudwatchlogs/emf_logs", tt.ID().String())
 	testCases := map[string]struct {
 		env     map[string]string
-		input   map[string]interface{}
-		want    awscloudwatchlogsexporter.Config
+		input   map[string]any
+		want    func() *awscloudwatchlogsexporter.Config
 		wantErr error
 	}{
-		"Emf": {
-			input: map[string]interface{}{
-				"logs": map[string]interface{}{
-					"metrics_collected": map[string]interface{}{
-						"emf": map[string]interface{}{},
+		"Basic": {
+			input: map[string]any{
+				"logs": map[string]any{
+					"metrics_collected": map[string]any{
+						"emf": map[string]any{},
 					},
 					"log_stream_name": "same random stream",
 				},
 			},
-			want: awscloudwatchlogsexporter.Config{
-				LogGroupName:  "emf/logs/default",
-				LogStreamName: "same random stream",
-				RawLog:        true,
+			want: func() *awscloudwatchlogsexporter.Config {
+				cfg := &awscloudwatchlogsexporter.Config{
+					LogGroupName:  "emf/logs/default",
+					LogStreamName: "same random stream",
+					RawLog:        true,
+					EmfOnly:       true,
+				}
+				cfg.AWSSessionSettings.CertificateFilePath = "/ca/bundle"
+				cfg.AWSSessionSettings.Region = "us-east-1"
+				cfg.AWSSessionSettings.RoleARN = "global_arn"
+				return cfg
 			},
 		},
 	}
@@ -50,10 +62,13 @@ func TestTranslator(t *testing.T) {
 				require.NotNil(t, got)
 				gotCfg, ok := got.(*awscloudwatchlogsexporter.Config)
 				require.True(t, ok)
-				assert.Equal(t, testCase.want.LogGroupName, gotCfg.LogGroupName)
-				assert.Equal(t, testCase.want.LogStreamName, gotCfg.LogStreamName)
-				assert.Equal(t, testCase.want.RawLog, gotCfg.RawLog)
-				assert.Equal(t, testCase.want.Region, gotCfg.Region)
+				wantCfg := testCase.want()
+				assert.Equal(t, wantCfg.LogGroupName, gotCfg.LogGroupName)
+				assert.Equal(t, wantCfg.LogStreamName, gotCfg.LogStreamName)
+				assert.Equal(t, wantCfg.RawLog, gotCfg.RawLog)
+				assert.Equal(t, wantCfg.EmfOnly, gotCfg.EmfOnly)
+				assert.Equal(t, wantCfg.Region, gotCfg.Region)
+				assert.Equal(t, wantCfg.Region, gotCfg.RoleARN)
 				assert.NotNil(t, gotCfg.MiddlewareID)
 				assert.Equal(t, "agenthealth/logs", gotCfg.MiddlewareID.String())
 			}
