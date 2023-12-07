ff201bd9ea9b106f2707cac7fa20076b2ac1e147
Dinakar Chappa <dchappa@amazon.com>
2023-12-07 17:02:56 -0500

    Validation no longer defaults to standard log group class on empty or invalid log group class

--- translator/translate/logs/logs_collected/files/collect_list/ruleLogGroupClass.go
+++ translator/translate/logs/logs_collected/files/collect_list/ruleLogGroupClass.go
@@ -1,26 +1,24 @@
-// // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// // SPDX-License-Identifier: MIT
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
 package collect_list
 
-//
-//import (
-//	"github.com/aws/amazon-cloudwatch-agent/tool/util"
-//	"github.com/aws/amazon-cloudwatch-agent/translator"
-//)
-//
-//const LogGroupClassSectionKey = "log_group_class"
-//
-//type LogGroupClass struct {
-//}
-//
-//func (f *LogGroupClass) ApplyRule(input interface{}) (returnKey string, returnVal interface{}) {
-//	_, returnVal = translator.DefaultLogGroupClassCase(LogGroupClassSectionKey, util.StandardLogGroupClass, input)
-//	returnKey = LogGroupClassSectionKey
-//	return
-//}
-//
-//func init() {
-//	l := new(LogGroupClass)
-//	r := []Rule{l}
-//	RegisterRule(LogGroupClassSectionKey, r)
-//}
+import (
+	"github.com/aws/amazon-cloudwatch-agent/translator"
+)
+
+const LogGroupClassSectionKey = "log_group_class"
+
+type LogGroupClass struct {
+}
+
+func (f *LogGroupClass) ApplyRule(input interface{}) (returnKey string, returnVal interface{}) {
+	_, returnVal = translator.DefaultLogGroupClassCase(LogGroupClassSectionKey, "", input)
+	returnKey = LogGroupClassSectionKey
+	return
+}
+
+func init() {
+	l := new(LogGroupClass)
+	r := []Rule{l}
+	RegisterRule(LogGroupClassSectionKey, r)
+}
