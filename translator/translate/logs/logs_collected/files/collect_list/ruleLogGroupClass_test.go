df28aa7232bebadc732be4a5c9791e9a69f6e0ba
Dinakar Chappa <dchappa@amazon.com>
2023-12-07 17:57:00 -0500

    small comment fix

--- translator/translate/logs/logs_collected/files/collect_list/ruleLogGroupClass_test.go
+++ translator/translate/logs/logs_collected/files/collect_list/ruleLogGroupClass_test.go
@@ -1,5 +1,5 @@
-// // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// // SPDX-License-Identifier: MIT
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
 package collect_list
 
 import (
