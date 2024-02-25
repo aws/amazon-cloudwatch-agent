2422927a588209bec1b9066bb97a0c6c4f229fd5
Hyunsoo Kim <hsookim@amazon.com>
2024-02-24 20:05:39 -0500

    Revert "remove gpu processor"
    This reverts commit 7a6784b24ef6a222f7f79ee1ad4b74909d3ccc2d.

---
+++ plugins/processors/gpu/processor.go
@@ -0,0 +1,111 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package gpu
+
+import (
+	"context"
+	"strings"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/pdata/pcommon"
+	"go.opentelemetry.io/collector/pdata/pmetric"
+	"go.uber.org/zap"
+)
+
+const (
+	gpuMetric = "_gpu_"
+)
+
+var defaultGpuLabels = []string{
+	"ClusterName",
+	"Namespace",
+	"Service",
+	"ContainerName",
+	"FullPodName",
+	"PodName",
+	"GpuDevice",
+}
+
+type gpuprocessor struct {
+	*Config
+	logger     *zap.Logger
+	cancelFunc context.CancelFunc
+	shutdownC  chan bool
+	started    bool
+}
+
+func newGpuProcessor(config *Config, logger *zap.Logger) *gpuprocessor {
+	_, cancel := context.WithCancel(context.Background())
+	d := &gpuprocessor{
+		Config:     config,
+		logger:     logger,
+		cancelFunc: cancel,
+	}
+	return d
+}
+
+func (d *gpuprocessor) processMetrics(ctx context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
+	if !d.started {
+		return pmetric.NewMetrics(), nil
+	}
+
+	rms := md.ResourceMetrics()
+	for i := 0; i < rms.Len(); i++ {
+		rs := rms.At(i)
+		ilms := rs.ScopeMetrics()
+		for j := 0; j < ilms.Len(); j++ {
+			ils := ilms.At(j)
+			metrics := ils.Metrics()
+			for k := 0; k < metrics.Len(); k++ {
+				m := metrics.At(k)
+				d.processMetricAttributes(ctx, m)
+			}
+		}
+	}
+	return md, nil
+}
+
+func (d *gpuprocessor) processMetricAttributes(_ context.Context, m pmetric.Metric) {
+	// only decorate GPU metrics
+	// another option is to separate GPU of its own pipeline to minimize extra processing of metrics
+	if !strings.Contains(m.Name(), gpuMetric) {
+		return
+	}
+
+	switch m.Type() {
+	case pmetric.MetricTypeGauge:
+		dps := m.Gauge().DataPoints()
+		for i := 0; i < dps.Len(); i++ {
+			addDefaultAttributes(dps.At(i).Attributes())
+		}
+	case pmetric.MetricTypeSum:
+		dps := m.Sum().DataPoints()
+		for i := 0; i < dps.Len(); i++ {
+			addDefaultAttributes(dps.At(i).Attributes())
+		}
+	default:
+		d.logger.Debug("Ignore unknown metric type", zap.String("type", m.Type().String()))
+	}
+}
+
+// adds empty string for default attributes since prometheus drops them during relabeling process
+func addDefaultAttributes(attributes pcommon.Map) {
+	for _, k := range defaultGpuLabels {
+		if _, ok := attributes.Get(k); !ok {
+			attributes.PutStr(k, "")
+		}
+	}
+}
+
+func (d *gpuprocessor) Shutdown(context.Context) error {
+	close(d.shutdownC)
+	d.cancelFunc()
+	return nil
+}
+
+func (d *gpuprocessor) Start(ctx context.Context, _ component.Host) error {
+	d.shutdownC = make(chan bool)
+	d.started = true
+	return nil
+}
