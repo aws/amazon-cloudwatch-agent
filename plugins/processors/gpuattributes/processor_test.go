a35fa32aa811d7d3e9ea566bedbfb731006f2c81
Hyunsoo Kim <884273+movence@users.noreply.github.com>
2024-04-11 15:19:52 -0400

    Drop container/pod level Nvidia GPU metrics when there is no active workload. (#1126)

--- plugins/processors/gpuattributes/processor_test.go
+++ plugins/processors/gpuattributes/processor_test.go
@@ -8,13 +8,10 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/grafana/regexp"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
 )
-
-var normalizedNameRegex = regexp.MustCompile("^(container|pod|node)_gpu_[a-z_]+$")
 
 func TestProcessMetrics(t *testing.T) {
 	logger, _ := zap.NewDevelopment()
@@ -24,12 +21,14 @@ func TestProcessMetrics(t *testing.T) {
 	testcases := map[string]struct {
 		resource string
 		metrics  pmetric.Metrics
+		wantCnt  int
 		want     map[string]string
 	}{
 		"nonNode": {
 			metrics: generateMetrics("prefix", map[string]string{
 				"ClusterName": "cluster",
 			}),
+			wantCnt: 1,
 			want: map[string]string{
 				"ClusterName": "cluster",
 			},
@@ -39,6 +38,7 @@ func TestProcessMetrics(t *testing.T) {
 				"ClusterName": "cluster",
 				"Drop":        "val",
 			}),
+			wantCnt: 1,
 			want: map[string]string{
 				"ClusterName": "cluster",
 			},
@@ -48,6 +48,7 @@ func TestProcessMetrics(t *testing.T) {
 				"ClusterName": "cluster",
 				"kubernetes":  "{\"host\":\"test\"}",
 			}),
+			wantCnt: 1,
 			want: map[string]string{
 				"ClusterName": "cluster",
 				"kubernetes":  "{\"host\":\"test\"}",
@@ -59,8 +60,51 @@ func TestProcessMetrics(t *testing.T) {
 				"Drop":        "val",
 				"kubernetes":  "{\"host\":\"test\",\"b\":\"2\"}",
 			}),
+			wantCnt: 1,
 			want: map[string]string{
 				"ClusterName": "cluster",
+				"kubernetes":  "{\"host\":\"test\"}",
+			},
+		},
+		"dropPodWithoutPodName": {
+			metrics: generateMetrics("pod", map[string]string{
+				"ClusterName": "cluster",
+				"kubernetes":  "{\"host\":\"test\",\"b\":\"2\"}",
+			}),
+			wantCnt: 0,
+			want:    map[string]string{},
+		},
+		"keepPodWithoutPodName": {
+			metrics: generateMetrics("pod", map[string]string{
+				"ClusterName": "cluster",
+				"PodName":     "pod",
+				"kubernetes":  "{\"host\":\"test\",\"b\":\"2\"}",
+			}),
+			wantCnt: 1,
+			want: map[string]string{
+				"ClusterName": "cluster",
+				"PodName":     "pod",
+				"kubernetes":  "{\"host\":\"test\"}",
+			},
+		},
+		"dropContainerWithoutPodName": {
+			metrics: generateMetrics("container", map[string]string{
+				"ClusterName": "cluster",
+				"kubernetes":  "{\"host\":\"test\",\"b\":\"2\"}",
+			}),
+			wantCnt: 0,
+			want:    map[string]string{},
+		},
+		"keepContainerWithoutPodName": {
+			metrics: generateMetrics("container", map[string]string{
+				"ClusterName": "cluster",
+				"PodName":     "pod",
+				"kubernetes":  "{\"host\":\"test\",\"b\":\"2\"}",
+			}),
+			wantCnt: 1,
+			want: map[string]string{
+				"ClusterName": "cluster",
+				"PodName":     "pod",
 				"kubernetes":  "{\"host\":\"test\"}",
 			},
 		},
@@ -69,12 +113,15 @@ func TestProcessMetrics(t *testing.T) {
 	for tname, tc := range testcases {
 		fmt.Printf("running %s\n", tname)
 		ms, _ := gp.processMetrics(ctx, tc.metrics)
-		attrs := ms.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0).Gauge().DataPoints().At(0).Attributes()
-		assert.Equal(t, len(tc.want), attrs.Len())
-		for k, v := range tc.want {
-			got, ok := attrs.Get(k)
-			assert.True(t, ok)
-			assert.Equal(t, v, got.Str())
+		assert.Equal(t, tc.wantCnt, ms.MetricCount())
+		if tc.wantCnt > 0 {
+			attrs := ms.ResourceMetrics().At(0).ScopeMetrics().At(0).Metrics().At(0).Gauge().DataPoints().At(0).Attributes()
+			assert.Equal(t, len(tc.want), attrs.Len())
+			for k, v := range tc.want {
+				got, ok := attrs.Get(k)
+				assert.True(t, ok)
+				assert.Equal(t, v, got.Str())
+			}
 		}
 	}
 }
