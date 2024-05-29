bfaa46cdadd84d6a87acd90f474645de628b0e18
Mitali Salvi <mitsalvi@amazon.com>
2024-05-29 14:25:03 -0400

    adding support for placeholders

--- translator/translate/otel/receiver/awscontainerinsight/translator.go
+++ translator/translate/otel/receiver/awscontainerinsight/translator.go
@@ -19,8 +19,10 @@ import (
 	"github.com/aws/amazon-cloudwatch-agent/translator/config"
 	"github.com/aws/amazon-cloudwatch-agent/translator/context"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/agent"
+	globallogs "github.com/aws/amazon-cloudwatch-agent/translator/translate/logs"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/logs/util"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+	logsutil "github.com/aws/amazon-cloudwatch-agent/translator/translate/util"
 )
 
 // container orchestrator keys
@@ -113,13 +115,9 @@ func (t *translator) Translate(conf *confmap.Conf) (component.Config, error) {
 			cfg.KubeConfigPath = kubeConfigPath
 		}
 
-		if hostIP, ok := common.GetString(conf, common.ConfigKey(common.LogsKey, common.MetricsCollectedKey, common.KubernetesKey, "host_ip")); ok {
-			cfg.HostIP = hostIP
-		}
+		t.setHostName(conf, cfg)
+		t.setHostIP(conf, cfg)
 
-		if hostName, ok := common.GetString(conf, common.ConfigKey(common.LogsKey, common.MetricsCollectedKey, common.KubernetesKey, "host_name")); ok {
-			cfg.HostName = hostName
-		}
 	}
 
 	cfg.PrefFullPodName = cfg.PrefFullPodName || common.GetOrDefaultBool(conf, common.ConfigKey(common.LogsKey, common.MetricsCollectedKey, common.KubernetesKey, common.PreferFullPodName), false)
@@ -140,6 +138,20 @@ func (t *translator) setClusterName(conf *confmap.Conf, cfg *awscontainerinsight
 		return errors.New("cluster name is not provided and was not auto-detected from EC2 tags")
 	}
 	return nil
+}
+
+func (t *translator) setHostIP(conf *confmap.Conf, cfg *awscontainerinsightreceiver.Config) {
+	hostIPKey := common.ConfigKey(common.LogsKey, common.MetricsCollectedKey, common.KubernetesKey, "host_ip")
+	if hostIP, ok := common.GetString(conf, hostIPKey); ok {
+		cfg.HostIP = logsutil.ResolvePlaceholder(hostIP, globallogs.GlobalLogConfig.MetadataInfo)
+	}
+}
+
+func (t *translator) setHostName(conf *confmap.Conf, cfg *awscontainerinsightreceiver.Config) {
+	hostNameKey := common.ConfigKey(common.LogsKey, common.MetricsCollectedKey, common.KubernetesKey, "host_name")
+	if hostName, ok := common.GetString(conf, hostNameKey); ok {
+		cfg.HostName = logsutil.ResolvePlaceholder(hostName, globallogs.GlobalLogConfig.MetadataInfo)
+	}
 }
 
 // getConfiguredContainerService gets the first found container service
