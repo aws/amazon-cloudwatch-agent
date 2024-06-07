7e99155ed71e3f64f640c85c7a05d7cf4fd99490
Paramadon <siprmp@amazon.com>
2024-06-07 15:06:30 -0400

    fixed indentation

--- translator/translate/metrics/metrics_collect/ethtool/ethtool.go
+++ translator/translate/metrics/metrics_collect/ethtool/ethtool.go
@@ -18,11 +18,12 @@ var ChildRule = map[string]translator.Rule{}
 //	        "bw_in_allowance_exceeded",
 //	        "bw_out_allowance_exceeded"
 //	    ],
-//		"append_dimensions":{
-//			key:value
-//		}
+//	    "append_dimensions":{
+//		key:value
+//	     }
 //
 // }
+
 const SectionKey_Ethtool = "ethtool"
 
 func RegisterRule(fieldname string, r translator.Rule) {
