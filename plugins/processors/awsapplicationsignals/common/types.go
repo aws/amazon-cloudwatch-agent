2396d12769576b548754ff7f04434d2c44bb9434
Mengyi Zhou <zmengyi@amazon.com>
2024-07-11 15:21:15 -0700

    Rename and group application signals attributes

--- plugins/processors/awsapplicationsignals/common/types.go
+++ plugins/processors/awsapplicationsignals/common/types.go
@@ -3,42 +3,54 @@
 
 package common
 
+// Metric attribute used as CloudWatch metric dimension.
 const (
-	MetricAttributeLocalService             = "Service"
-	MetricAttributeLocalOperation           = "Operation"
-	MetricAttributeEnvironment              = "Environment"
-	MetricAttributeRemoteService            = "RemoteService"
-	MetricAttributeRemoteEnvironment        = "RemoteEnvironment"
-	MetricAttributeRemoteOperation          = "RemoteOperation"
-	MetricAttributeRemoteResourceIdentifier = "RemoteResourceIdentifier"
-	MetricAttributeRemoteResourceType       = "RemoteResourceType"
-	MetricAttributeRemoteDbUser             = "RemoteDbUser"
+	CWMetricAttributeLocalService             = "Service"
+	CWMetricAttributeLocalOperation           = "Operation"
+	CWMetricAttributeEnvironment              = "Environment"
+	CWMetricAttributeRemoteService            = "RemoteService"
+	CWMetricAttributeRemoteEnvironment        = "RemoteEnvironment"
+	CWMetricAttributeRemoteOperation          = "RemoteOperation"
+	CWMetricAttributeRemoteResourceIdentifier = "RemoteResourceIdentifier"
+	CWMetricAttributeRemoteResourceType       = "RemoteResourceType"
 )
 
+// Platform attribute used as CloudWatch EMF log field and X-Ray trace annotation.
 const (
+	AttributePlatformType        = "PlatformType"
 	AttributeEKSClusterName      = "EKS.Cluster"
 	AttributeK8SClusterName      = "K8s.Cluster"
 	AttributeK8SNamespace        = "K8s.Namespace"
+	AttributeK8SWorkload         = "K8s.Workload"
+	AttributeK8SPod              = "K8s.Pod"
 	AttributeEC2AutoScalingGroup = "EC2.AutoScalingGroup"
 	AttributeEC2InstanceId       = "EC2.InstanceId"
 	AttributeHost                = "Host"
-	AttributePlatformType        = "PlatformType"
-	AttributeTelemetrySDK        = "Telemetry.SDK"
-	AttributeTelemetryAgent      = "Telemetry.Agent"
-	AttributeTelemetrySource     = "Telemetry.Source"
+)
+
+// Telemetry attributes used as CloudWatch EMF log fields.
+const (
+	MetricAttributeTelemetrySDK    = "Telemetry.SDK"
+	MetricAttributeTelemetryAgent  = "Telemetry.Agent"
+	MetricAttributeTelemetrySource = "Telemetry.Source"
+)
+
+// Resource attributes used as CloudWatch EMF log fields.
+const (
+	MetricAttributeRemoteDbUser = "RemoteDbUser"
 )
 
 const (
 	AttributeTmpReserved = "aws.tmp.reserved"
 )
 
-var IndexableMetricAttributes = []string{
-	MetricAttributeLocalService,
-	MetricAttributeLocalOperation,
-	MetricAttributeEnvironment,
-	MetricAttributeRemoteService,
-	MetricAttributeRemoteEnvironment,
-	MetricAttributeRemoteOperation,
-	MetricAttributeRemoteResourceIdentifier,
-	MetricAttributeRemoteResourceType,
+var CWMetricAttributes = []string{
+	CWMetricAttributeLocalService,
+	CWMetricAttributeLocalOperation,
+	CWMetricAttributeEnvironment,
+	CWMetricAttributeRemoteService,
+	CWMetricAttributeRemoteEnvironment,
+	CWMetricAttributeRemoteOperation,
+	CWMetricAttributeRemoteResourceIdentifier,
+	CWMetricAttributeRemoteResourceType,
 }
