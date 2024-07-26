ba867d1a27eb80913169af58c46b8ddae4e87398
Aditya Purang <puranga@amazon.com>
2024-07-26 11:40:09 +0000

    make fmt

--- plugins/processors/gpuattributes/internal/metricFilters/gpumetricfilters.go
+++ plugins/processors/gpuattributes/internal/metricFilters/gpumetricfilters.go
@@ -1,3 +1,6 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
 package metricFilters
 
 import "github.com/aws/amazon-cloudwatch-agent/internal/containerinsightscommon"
