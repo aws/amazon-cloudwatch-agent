7b33b2b6c946464cd010fb4d274afb606a8fc215
Aditya Purang <44022838+aditya-purang@users.noreply.github.com>
2024-08-02 16:34:47 +0100

    minor refactor

--- plugins/processors/gpuattributes/internal/awsneuron_metric_checker.go
+++ plugins/processors/gpuattributes/internal/awsneuron_metric_checker.go
@@ -7,6 +7,10 @@ import (
 	"regexp"
 )
 
+const (
+	PROCESSED_NEURON_METRIC_PATTERN = "^(container|node|pod)_(neuroncore_|neurondevice_).*|^node_neuron_.*"
+)
+
 type AwsNeuronMetricChecker struct {
 }
 
@@ -15,7 +19,7 @@ func NewAwsNeuronMetricChecker() *AwsNeuronMetricChecker {
 }
 
 func (md *AwsNeuronMetricChecker) IsProcessedNeuronMetric(name string) bool {
-	matched, err := regexp.MatchString("^(container|node|pod)_(neuroncore_|neurondevice_).*|^node_neuron_.*", name)
+	matched, err := regexp.MatchString(PROCESSED_NEURON_METRIC_PATTERN, name)
 	if err != nil {
 		print(err)
 		return false
