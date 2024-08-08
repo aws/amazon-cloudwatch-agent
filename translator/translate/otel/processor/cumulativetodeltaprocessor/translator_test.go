d0ec54d655bcec6afd52ceaf667162aa3d2d7d31
Mitali Salvi <mitsalvi@amazon.com>
2024-08-08 15:43:27 -0400

    Revert "Change cumulative to delta conversion to drop initial value (#1278)"
    This reverts commit dcc2f3317e43be349749caac53046acebd74fbe4.

--- translator/translate/otel/processor/cumulativetodeltaprocessor/translator_test.go
+++ translator/translate/otel/processor/cumulativetodeltaprocessor/translator_test.go
@@ -8,7 +8,6 @@ import (
 	"testing"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/cumulativetodeltaprocessor"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.opentelemetry.io/collector/confmap"
 
@@ -19,50 +18,47 @@ func TestTranslator(t *testing.T) {
 	cdpTranslator := NewTranslator(common.WithName("test"), WithDiskIONetKeys())
 	require.EqualValues(t, "cumulativetodelta/test", cdpTranslator.ID().String())
 	testCases := map[string]struct {
-		input   map[string]any
-		want    map[string]any
+		input   map[string]interface{}
+		want    *cumulativetodeltaprocessor.Config
 		wantErr error
 	}{
 		"GenerateDeltaProcessorConfigWithCPU": {
-			input: map[string]any{
-				"metrics": map[string]any{
-					"metrics_collected": map[string]any{
-						"cpu": map[string]any{},
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"cpu": map[string]interface{}{},
 					},
 				},
 			},
 			wantErr: &common.MissingKeyError{ID: cdpTranslator.ID(), JsonKey: fmt.Sprint(diskioKey, " or ", netKey)},
 		},
 		"GenerateDeltaProcessorConfigWithNet": {
-			input: map[string]any{
-				"metrics": map[string]any{
-					"metrics_collected": map[string]any{
-						"net": map[string]any{},
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"net": map[string]interface{}{},
 					},
 				},
 			},
-			want: map[string]any{
-				"initial_value": "drop",
+			want: &cumulativetodeltaprocessor.Config{
+				Include: cumulativetodeltaprocessor.MatchMetrics{},
+				Exclude: cumulativetodeltaprocessor.MatchMetrics{},
 			},
 		},
 		"GenerateDeltaProcessorConfigWithDiskIO": {
-			input: map[string]any{
-				"metrics": map[string]any{
-					"metrics_collected": map[string]any{
-						"diskio": map[string]any{},
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"diskio": map[string]interface{}{},
 					},
 				},
 			},
-			want: map[string]any{
-				"exclude": map[string]any{
-					"match_type": "strict",
-					"metrics":    []string{"iops_in_progress", "diskio_iops_in_progress"},
-				},
-				"initial_value": "drop",
+			want: &cumulativetodeltaprocessor.Config{
+				Include: cumulativetodeltaprocessor.MatchMetrics{},
+				Exclude: cumulativetodeltaprocessor.MatchMetrics{Metrics: []string{"iops_in_progress", "diskio_iops_in_progress"}},
 			},
 		},
 	}
-	factory := cumulativetodeltaprocessor.NewFactory()
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
 			conf := confmap.NewFromStringMap(testCase.input)
@@ -72,10 +68,8 @@ func TestTranslator(t *testing.T) {
 				require.NotNil(t, got)
 				gotCfg, ok := got.(*cumulativetodeltaprocessor.Config)
 				require.True(t, ok)
-				wantCfg := factory.CreateDefaultConfig()
-				wantConf := confmap.NewFromStringMap(testCase.want)
-				require.NoError(t, wantConf.Unmarshal(&wantCfg))
-				assert.Equal(t, wantCfg, gotCfg)
+				require.Equal(t, testCase.want.Include.Metrics, gotCfg.Include.Metrics)
+				require.Equal(t, testCase.want.Exclude.Metrics, gotCfg.Exclude.Metrics)
 			}
 		})
 	}
