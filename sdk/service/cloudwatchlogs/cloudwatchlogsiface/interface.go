b4d1a93ef83458c61393cf2ccaf69171a3bc8090
zhihonl <61301537+zhihonl@users.noreply.github.com>
2024-08-20 16:43:08 -0400

    Fix CloudWatch Log SDK unit tests by referencing correct dependency (#784)

--- sdk/service/cloudwatchlogs/cloudwatchlogsiface/interface.go
+++ sdk/service/cloudwatchlogs/cloudwatchlogsiface/interface.go
@@ -11,7 +11,8 @@ package cloudwatchlogsiface
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/request"
-	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
+
+	"github.com/aws/amazon-cloudwatch-agent/sdk/service/cloudwatchlogs"
 )
 
 // CloudWatchLogsAPI provides an interface to enable mocking the
