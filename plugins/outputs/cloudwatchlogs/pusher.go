af040c59fe84f9aaa9146f750df3c8f46456dd03
Rick Rossi <dricross@amazon.com>
2024-08-27 11:09:51 -0400

    Rework detection of certain error types

--- plugins/outputs/cloudwatchlogs/pusher.go
+++ plugins/outputs/cloudwatchlogs/pusher.go
@@ -4,16 +4,16 @@
 package cloudwatchlogs
 
 import (
-	"errors"
 	"math/rand"
 	"net"
 	"sort"
+	"strings"
 	"sync"
-	"syscall"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/cloudwatchlogs"
 	"github.com/influxdata/telegraf"
 
@@ -338,30 +338,40 @@ func (p *pusher) send() {
 
 }
 
-// retryWaitShort returns a duration to wait before retrying a request.
-// This implements an exponential backoff strategy with jitter.
-// The base wait time is 200 milliseconds, and the maximum wait time is 1 minute (jittered).
-// The wait time is capped at 1 minute if the retry count exceeds 5.
+const (
+	// baseRetryDelayShort is the base retry delay for short retry strategy
+	baseRetryDelayShort = 200 * time.Millisecond
+
+	// baseRetryDelayLong is the base retry delay for long retry strategy
+	baseRetryDelayLong = 2 * time.Second
+
+	// numBackoffRetriesShort is the maximum number of consecutive retries using the short retry strategy before using
+	// the maxRetryDelay
+	numBackoffRetriesShort = 5
+
+	// numBackoffRetriesLong is the maximum number of consecutive retries using the long retry strategy before using
+	// the maxRetryDelay
+	numBackoffRetriesLong = 2
+
+	// maxRetryDelay is the maximum retry delay for the either retry strategy
+	maxRetryDelay = 1 * time.Minute
+)
+
+// retryWaitShort returns a duration to wait before retrying a request using the short retry strategy
 func retryWaitShort(retryCount int) time.Duration {
-	const base = 200 * time.Millisecond
-	// Max wait time is 1 minute (jittered)
-	d := 1 * time.Minute
-	if retryCount < 5 {
-		d = base * time.Duration(1<<int64(retryCount))
-	}
-	return time.Duration(seededRand.Int63n(int64(d/2)) + int64(d/2))
+	return retryWait(baseRetryDelayShort, numBackoffRetriesShort, retryCount)
 }
 
-// retryWaitLong returns a duration to wait before retrying a request.
-// This implements an exponential backoff strategy with jitter.
-// The base wait time is 2 seconds, and the maximum wait time is 1 minute (jittered).
-// The wait time is capped at 1 minute if the long-retry count exceeds 2.
+// retryWaitLong returns a duration to wait before retrying a request using the long retry strategy.
+// this strategy is used for errors that should not be retried too quickly
 func retryWaitLong(retryCount int) time.Duration {
-	const base = 2 * time.Second
-	// Max wait time is 1 minute (jittered)
-	d := 1 * time.Minute
-	if retryCount < 2 {
-		d = base * time.Duration(1<<int64(retryCount))
+	return retryWait(baseRetryDelayLong, numBackoffRetriesLong, retryCount)
+}
+
+func retryWait(baseRetryDelay time.Duration, maxBackoffRetries int, retryCount int) time.Duration {
+	d := maxRetryDelay
+	if retryCount < maxBackoffRetries {
+		d = baseRetryDelay * time.Duration(1<<int64(retryCount))
 	}
 	return time.Duration(seededRand.Int63n(int64(d/2)) + int64(d/2))
 }
@@ -370,7 +380,7 @@ func retryWaitLong(retryCount int) time.Duration {
 // returns an error. A short retry strategy should be used for most errors, while a long retry strategy is used for
 // errors where retrying too quickly could cause excessive strain on the backend servers.
 //
-// Specifically, use the long retry strategy for the following PutLogEvent errors:
+// Specifically, use the long retry strategy for the following PutLogEvents errors:
 //   - 500 (InternalFailure)
 //   - 503 (ServiceUnavailable)
 //   - Connection Refused
@@ -378,24 +388,27 @@ func retryWaitLong(retryCount int) time.Duration {
 //   - Connection Timeout
 //   - Throttling
 func chooseRetryWaitStrategy(err error) retryWaitStrategy {
-	if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-		return retryLong
-	}
-
-	// Connection reset/refused are actually syscalls
-	if errors.Is(err, syscall.ECONNRESET) || errors.Is(err, syscall.ECONNREFUSED) {
+	if isErrConnectionTimeout(err) || isErrConnectionReset(err) || isErrConnectionRefused(err) || request.IsErrorThrottle(err) {
 		return retryLong
 	}
 
 	// Check AWS Error codes if available
 	if awsErr, ok := err.(awserr.Error); ok {
-		if awsErr.Code() == cloudwatchlogs.ErrCodeServiceUnavailableException || awsErr.Code() == cloudwatchlogs.ErrCodeThrottlingException {
+		switch awsErr.Code() {
+		case
+			cloudwatchlogs.ErrCodeServiceUnavailableException,
+			cloudwatchlogs.ErrCodeThrottlingException,
+			"RequestTimeout",
+			request.ErrCodeResponseTimeout:
 			return retryLong
 		}
 
 		// Check HTTP status codes if available
 		if requestFailure, ok := err.(awserr.RequestFailure); ok {
-			if requestFailure.StatusCode() == 500 || requestFailure.StatusCode() == 503 {
+			switch requestFailure.StatusCode() {
+			case
+				500, // internal failure
+				503: // service unavailable
 				return retryLong
 			}
 		}
@@ -403,6 +416,29 @@ func chooseRetryWaitStrategy(err error) retryWaitStrategy {
 
 	// Otherwise, default to short retry strategy
 	return retryShort
+}
+
+func isErrConnectionTimeout(err error) bool {
+	netErr, ok := err.(net.Error)
+	return ok && netErr.Timeout()
+}
+
+func isErrConnectionReset(err error) bool {
+	if strings.Contains(err.Error(), "read: connection reset") {
+		return false
+	}
+
+	if strings.Contains(err.Error(), "use of closed network connection") ||
+		strings.Contains(err.Error(), "connection reset") ||
+		strings.Contains(err.Error(), "broken pipe") {
+		return true
+	}
+
+	return false
+}
+
+func isErrConnectionRefused(err error) bool {
+	return strings.Contains(err.Error(), "connection refused")
 }
 
 func (p *pusher) createLogGroupAndStream() error {
