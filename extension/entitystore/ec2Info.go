a19df6ffac4b8fda6dfd207df21aae9d40e0f460
POOJA REDDY NATHALA <poojardy@amazon.com>
2024-11-06 15:07:42 -0500

    Revert "Do not send AccountID as KeyAttribute for entities in PutLogEvents requests" and add unit tests  (#1411)
    Co-authored-by: zhihonl <61301537+zhihonl@users.noreply.github.com>

--- extension/entitystore/ec2Info.go
+++ extension/entitystore/ec2Info.go
@@ -76,7 +76,7 @@ func (ei *EC2Info) setInstanceIDAccountID() error {
 	for {
 		metadataDoc, err := ei.metadataProvider.Get(context.Background())
 		if err != nil {
-			ei.logger.Warn("Failed to get Instance ID / Account ID through metadata provider", zap.Error(err))
+			ei.logger.Debug("Failed to get Instance ID / Account ID through metadata provider", zap.Error(err))
 			wait := time.NewTimer(1 * time.Minute)
 			select {
 			case <-ei.done:
@@ -122,7 +122,7 @@ func (ei *EC2Info) setAutoScalingGroup() error {
 		}
 
 		if err := ei.retrieveAsgName(); err != nil {
-			ei.logger.Warn("Unable to fetch instance tags with imds", zap.Int("retry", retry), zap.Error(err))
+			ei.logger.Debug("Unable to fetch instance tags with imds", zap.Int("retry", retry), zap.Error(err))
 		} else {
 			ei.logger.Debug("Retrieval of auto-scaling group tags succeeded")
 			return nil
