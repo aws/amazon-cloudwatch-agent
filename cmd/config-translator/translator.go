397f4a6569b9d82e47ef76dfb66ba818b202b0ae
Chad Patel <patchad@amazon.com>
2024-12-03 11:43:19 -0600

    work in progress -- refactor config-translate in to amazon-cloudwatch-agent to save 114M on disk and 35MB on the RPM

--- cmd/config-translator/translator.go
+++ cmd/config-translator/translator.go
@@ -1,137 +1,61 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT
-
 package main
 
 import (
-	"errors"
 	"flag"
+	"fmt"
 	"log"
 	"os"
-	"os/user"
-	"path/filepath"
+	"os/exec"
 
-	"github.com/aws/amazon-cloudwatch-agent/cfg/commonconfig"
-	userutil "github.com/aws/amazon-cloudwatch-agent/internal/util/user"
-	"github.com/aws/amazon-cloudwatch-agent/translator"
-	"github.com/aws/amazon-cloudwatch-agent/translator/cmdutil"
-	"github.com/aws/amazon-cloudwatch-agent/translator/context"
-	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/pipeline"
-	translatorUtil "github.com/aws/amazon-cloudwatch-agent/translator/util"
+	"github.com/aws/amazon-cloudwatch-agent/tool/paths"
 )
 
-const (
-	exitErrorMessage   = "Configuration validation first phase failed. Agent version: %v. Verify the JSON input is only using features supported by this version.\n"
-	exitSuccessMessage = "Configuration validation first phase succeeded"
-	version            = "1.0"
-	envConfigFileName  = "env-config.json"
-	yamlConfigFileName = "amazon-cloudwatch-agent.yaml"
-)
-
-func initFlags() {
-	var inputOs = flag.String("os", "", "Please provide the os preference, valid value: windows/linux.")
-	var inputJsonFile = flag.String("input", "", "Please provide the path of input agent json config file")
-	var inputJsonDir = flag.String("input-dir", "", "Please provide the path of input agent json config directory.")
-	var inputTomlFile = flag.String("output", "", "Please provide the path of the output CWAgent config file")
-	var inputMode = flag.String("mode", "ec2", "Please provide the mode, i.e. ec2, onPremise, onPrem, auto")
-	var inputConfig = flag.String("config", "", "Please provide the common-config file")
-	var multiConfig = flag.String("multi-config", "remove", "valid values: default, append, remove")
-	flag.Parse()
-
-	ctx := context.CurrentContext()
-	ctx.SetOs(*inputOs)
-	ctx.SetInputJsonFilePath(*inputJsonFile)
-	ctx.SetInputJsonDirPath(*inputJsonDir)
-	ctx.SetMultiConfig(*multiConfig)
-	ctx.SetOutputTomlFilePath(*inputTomlFile)
-
-	if *inputConfig != "" {
-		f, err := os.Open(*inputConfig)
-		if err != nil {
-			log.Fatalf("E! Failed to open common-config file %s with error: %v", *inputConfig, err)
-		}
-		defer f.Close()
-		conf, err := commonconfig.Parse(f)
-		if err != nil {
-			log.Fatalf("E! Failed to parse common-config file %s with error: %v", *inputConfig, err)
-		}
-		ctx.SetCredentials(conf.CredentialsMap())
-		ctx.SetProxy(conf.ProxyMap())
-		ctx.SetSSL(conf.SSLMap())
-		translatorUtil.LoadImdsRetries(conf.IMDS)
-	}
-	translatorUtil.SetProxyEnv(ctx.Proxy())
-	translatorUtil.SetSSLEnv(ctx.SSL())
-
-	mode := translatorUtil.DetectAgentMode(*inputMode)
-	ctx.SetMode(mode)
-	ctx.SetKubernetesMode(translatorUtil.DetectKubernetesMode(mode))
+type flagDef struct {
+	name        string
+	value       *string
+	defaultVal  string
+	description string
 }
 
-/**
- *	config-translator --input ${JSON} --input-dir ${JSON_DIR} --output ${TOML} --mode ${param_mode} --config ${COMMON_CONFIG}
- *  --multi-config [default|append|remove]
- *
- *		multi-config:
- *			default:	only process .tmp files
- *			append:		process both existing files and .tmp files
- *			remove:		only process existing files
- */
 func main() {
-	initFlags()
-	defer func() {
-		if r := recover(); r != nil {
-			// Only emit error message if panic content is string(pre-checked)
-			// Not emitting the non-handled error message for now, we don't want to show non-user-friendly error message to customer
-			if val, ok := r.(string); ok {
-				log.Println(val)
-			}
-			//If the Input JSON config file is invalid, output all the error path and error messages.
-			for _, errMessage := range translator.ErrorMessages {
-				log.Println(errMessage)
-			}
-			log.Printf(exitErrorMessage, version)
-			os.Exit(1)
-		}
-	}()
-	ctx := context.CurrentContext()
+	log.Printf("Starting config-translator, this will map back to a call to amazon-cloudwatch-agent")
 
-	mergedJsonConfigMap, err := cmdutil.GenerateMergedJsonConfigMap(ctx)
-	if err != nil {
-		log.Panicf("E! Failed to generate merged json config: %v", err)
+	flags := []flagDef{
+		{"os", nil, "", "Please provide the os preference, valid value: windows/linux."},
+		{"input", nil, "", "Please provide the path of input agent json config file"},
+		{"input-dir", nil, "", "Please provide the path of input agent json config directory."},
+		{"output", nil, "", "Please provide the path of the output CWAgent config file"},
+		{"mode", nil, "ec2", "Please provide the mode, i.e. ec2, onPremise, onPrem, auto"},
+		{"config", nil, "", "Please provide the common-config file"},
+		{"multi-config", nil, "remove", "valid values: default, append, remove"},
 	}
 
-	if !ctx.RunInContainer() {
-		// run as user only applies to non container situation.
-		current, err := user.Current()
-		if err == nil && current.Name == "root" {
-			runAsUser, err := userutil.DetectRunAsUser(mergedJsonConfigMap)
-			if err != nil {
-				log.Panic("E! Failed to detectRunAsUser")
-			}
-			cmdutil.VerifyCredentials(ctx, runAsUser)
+	for i := range flags {
+		flags[i].value = flag.String(flags[i].name, flags[i].defaultVal, flags[i].description)
+	}
+	flag.Parse()
+
+	args := []string{"-config-translator"}
+	for _, f := range flags {
+		if *f.value != "" {
+			// prefix ct so we do not accidentally overlap with other agent flags
+			args = append(args, fmt.Sprintf("-ct-%s", f.name), *f.value)
 		}
 	}
 
-	tomlConfigPath := cmdutil.GetTomlConfigPath(ctx.OutputTomlFilePath())
-	tomlConfigDir := filepath.Dir(tomlConfigPath)
-	yamlConfigPath := filepath.Join(tomlConfigDir, yamlConfigFileName)
-	tomlConfig, err := cmdutil.TranslateJsonMapToTomlConfig(mergedJsonConfigMap)
+	log.Printf("Executing %s with arguments: %v", paths.AgentBinaryPath, args)
+
+	cmd := exec.Command(paths.AgentBinaryPath, args...)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
 	if err != nil {
-		log.Panicf("E! Failed to generate TOML configuration validation content: %v", err)
+		if exitErr, ok := err.(*exec.ExitError); ok {
+			log.Panicf("E! Translation process exited with non-zero status: %d, err: %v", exitErr.ExitCode(), exitErr)
+		}
+		log.Panicf("E! Translation process failed. Error: %v", err)
+		os.Exit(1)
 	}
-	yamlConfig, err := cmdutil.TranslateJsonMapToYamlConfig(mergedJsonConfigMap)
-	if err != nil && !errors.Is(err, pipeline.ErrNoPipelines) {
-		log.Panicf("E! Failed to generate YAML configuration validation content: %v", err)
-	}
-	if err = cmdutil.ConfigToTomlFile(tomlConfig, tomlConfigPath); err != nil {
-		log.Panicf("E! Failed to create the configuration TOML validation file: %v", err)
-	}
-	if err = cmdutil.ConfigToYamlFile(yamlConfig, yamlConfigPath); err != nil {
-		log.Panicf("E! Failed to create the configuration YAML validation file: %v", err)
-	}
-	log.Println(exitSuccessMessage)
-	// Put env config into the same folder as the toml config
-	envConfigPath := filepath.Join(tomlConfigDir, envConfigFileName)
-	cmdutil.TranslateJsonMapToEnvConfigFile(mergedJsonConfigMap, envConfigPath)
 }
---
+++ translator/cmdutil/translator.go
@@ -0,0 +1,120 @@
+package cmdutil
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"path/filepath"
+
+	"github.com/aws/amazon-cloudwatch-agent/cfg/commonconfig"
+	userutil "github.com/aws/amazon-cloudwatch-agent/internal/util/user"
+	"github.com/aws/amazon-cloudwatch-agent/translator"
+
+	"github.com/aws/amazon-cloudwatch-agent/translator/context"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/pipeline"
+	translatorUtil "github.com/aws/amazon-cloudwatch-agent/translator/util"
+)
+
+const (
+	exitErrorMessage   = "Configuration validation first phase failed. Agent version: %v. Verify the JSON input is only using features supported by this version.\n"
+	exitSuccessMessage = "Configuration validation first phase succeeded"
+	version            = "1.0"
+	envConfigFileName  = "env-config.json"
+	yamlConfigFileName = "amazon-cloudwatch-agent.yaml"
+)
+
+type ConfigTranslator struct {
+	ctx *context.Context
+}
+
+func NewConfigTranslator(inputOs, inputJsonFile, inputJsonDir, inputTomlFile, inputMode, inputConfig, multiConfig string) (*ConfigTranslator, error) {
+	ct := ConfigTranslator{
+		ctx: context.CurrentContext(),
+	}
+
+	ct.ctx.SetOs(inputOs)
+	ct.ctx.SetInputJsonFilePath(inputJsonFile)
+	ct.ctx.SetInputJsonDirPath(inputJsonDir)
+	ct.ctx.SetMultiConfig(multiConfig)
+	ct.ctx.SetOutputTomlFilePath(inputTomlFile)
+
+	if inputConfig != "" {
+		f, err := os.Open(inputConfig)
+		if err != nil {
+			return nil, fmt.Errorf("failed to open common-config file %s with error: %v", inputConfig, err)
+		}
+		defer f.Close()
+		conf, err := commonconfig.Parse(f)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse common-config file %s with error: %v", inputConfig, err)
+		}
+		ct.ctx.SetCredentials(conf.CredentialsMap())
+		ct.ctx.SetProxy(conf.ProxyMap())
+		ct.ctx.SetSSL(conf.SSLMap())
+		translatorUtil.LoadImdsRetries(conf.IMDS)
+	}
+	translatorUtil.SetProxyEnv(ct.ctx.Proxy())
+	translatorUtil.SetSSLEnv(ct.ctx.SSL())
+
+	mode := translatorUtil.DetectAgentMode(inputMode)
+	ct.ctx.SetMode(mode)
+	ct.ctx.SetKubernetesMode(translatorUtil.DetectKubernetesMode(mode))
+
+	return &ct, nil
+}
+
+func (ct *ConfigTranslator) Translate() error {
+	defer func() {
+		if r := recover(); r != nil {
+			if val, ok := r.(string); ok {
+				log.Println(val)
+			}
+			for _, errMessage := range translator.ErrorMessages {
+				log.Println(errMessage)
+			}
+			log.Printf(exitErrorMessage, version)
+		}
+	}()
+
+	mergedJsonConfigMap, err := GenerateMergedJsonConfigMap(ct.ctx)
+	if err != nil {
+		return fmt.Errorf("failed to generate merged json config: %v", err)
+	}
+
+	if !ct.ctx.RunInContainer() {
+		current, err := user.Current()
+		if err == nil && current.Name == "****" {
+			runAsUser, err := userutil.DetectRunAsUser(mergedJsonConfigMap)
+			if err != nil {
+				return fmt.Errorf("failed to detectRunAsUser")
+			}
+			VerifyCredentials(ct.ctx, runAsUser)
+		}
+	}
+
+	tomlConfigPath := GetTomlConfigPath(ct.ctx.OutputTomlFilePath())
+	tomlConfigDir := filepath.Dir(tomlConfigPath)
+	yamlConfigPath := filepath.Join(tomlConfigDir, yamlConfigFileName)
+	tomlConfig, err := TranslateJsonMapToTomlConfig(mergedJsonConfigMap)
+	if err != nil {
+		return fmt.Errorf("failed to generate TOML configuration validation content: %v", err)
+	}
+	yamlConfig, err := TranslateJsonMapToYamlConfig(mergedJsonConfigMap)
+	if err != nil && !errors.Is(err, pipeline.ErrNoPipelines) {
+		return fmt.Errorf("failed to generate YAML configuration validation content: %v", err)
+	}
+	if err = ConfigToTomlFile(tomlConfig, tomlConfigPath); err != nil {
+		return fmt.Errorf("failed to create the configuration TOML validation file: %v", err)
+	}
+	if err = ConfigToYamlFile(yamlConfig, yamlConfigPath); err != nil {
+		return fmt.Errorf("failed to create the configuration YAML validation file: %v", err)
+	}
+	log.Println(exitSuccessMessage)
+
+	envConfigPath := filepath.Join(tomlConfigDir, envConfigFileName)
+	TranslateJsonMapToEnvConfigFile(mergedJsonConfigMap, envConfigPath)
+
+	return nil
+}
