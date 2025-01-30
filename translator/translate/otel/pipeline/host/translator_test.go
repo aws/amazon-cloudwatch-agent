77aa32a3ce8b249df0b52888afd7d45ac936616a
Dominic Sciascia <duhminick@users.noreply.github.com>
2025-01-30 15:02:57 -0500

    [OTLP] add entity processor to the otlp metrics and logs pipeline (#1504)

--- translator/translate/otel/pipeline/host/translator_test.go
+++ translator/translate/otel/pipeline/host/translator_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/aws/amazon-cloudwatch-agent/translator/config"
 	"github.com/aws/amazon-cloudwatch-agent/translator/context"
 	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+	"github.com/aws/amazon-cloudwatch-agent/translator/util/ecsutil"
 )
 
 type testTranslator struct {
@@ -44,7 +45,8 @@ func TestTranslator(t *testing.T) {
 		pipelineName   string
 		destination    string
 		mode           string
-		runInContainer bool
+		kubernetesMode string
+		isECS          bool
 		want           *want
 		wantErr        error
 	}{
@@ -80,6 +82,119 @@ func TestTranslator(t *testing.T) {
 				extensions: []string{"agenthealth/metrics", "agenthealth/statuscode"},
 			},
 		},
+		"WithOtlpMetricsEC2": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName: common.PipelineNameHostOtlpMetrics,
+			mode:         config.ModeEC2,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics"},
+				exporters:  []string{"awscloudwatch"},
+				extensions: []string{"agenthealth/metrics", "agenthealth/statuscode"},
+			},
+		},
+		"WithOtlpMetricsECS": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName: common.PipelineNameHostOtlpMetrics,
+			mode:         config.ModeEC2,
+			isECS:        true,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics"},
+				exporters:  []string{"awscloudwatch"},
+				extensions: []string{"agenthealth/metrics", "agenthealth/statuscode"},
+			},
+		},
+		"WithOtlpMetricsKubernetes": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName:   common.PipelineNameHostOtlpMetrics,
+			kubernetesMode: config.ModeK8sEC2,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics", "awsentity/service/otlp"},
+				exporters:  []string{"awscloudwatch"},
+				extensions: []string{"agenthealth/metrics", "agenthealth/statuscode"},
+			},
+		},
+		"WithOtlpMetrics/CloudWatchLogsEC2": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName: common.PipelineNameHostOtlpMetrics,
+			destination:  common.CloudWatchLogsKey,
+			mode:         config.ModeEC2,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics/cloudwatchlogs",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics/cloudwatchlogs", "batch/hostOtlpMetrics/cloudwatchlogs"},
+				exporters:  []string{"awsemf"},
+				extensions: []string{"agenthealth/logs", "agenthealth/statuscode"},
+			},
+		},
+		"WithOtlpMetrics/CloudWatchLogsECS": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName: common.PipelineNameHostOtlpMetrics,
+			destination:  common.CloudWatchLogsKey,
+			mode:         config.ModeEC2,
+			isECS:        true,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics/cloudwatchlogs",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics/cloudwatchlogs", "batch/hostOtlpMetrics/cloudwatchlogs"},
+				exporters:  []string{"awsemf"},
+				extensions: []string{"agenthealth/logs", "agenthealth/statuscode"},
+			},
+		},
+		"WithOtlpMetrics/CloudWatchLogsKubernetes": {
+			input: map[string]interface{}{
+				"metrics": map[string]interface{}{
+					"metrics_collected": map[string]interface{}{
+						"otlp": map[string]interface{}{},
+					},
+				},
+			},
+			pipelineName:   common.PipelineNameHostOtlpMetrics,
+			destination:    common.CloudWatchLogsKey,
+			kubernetesMode: config.ModeK8sEC2,
+			want: &want{
+				pipelineID: "metrics/hostOtlpMetrics/cloudwatchlogs",
+				receivers:  []string{"nop", "other"},
+				processors: []string{"cumulativetodelta/hostOtlpMetrics/cloudwatchlogs", "awsentity/service/otlp", "batch/hostOtlpMetrics/cloudwatchlogs"},
+				exporters:  []string{"awsemf"},
+				extensions: []string{"agenthealth/logs", "agenthealth/statuscode"},
+			},
+		},
 		"WithMetricsKeyStatsD": {
 			input: map[string]interface{}{
 				"metrics": map[string]interface{}{
@@ -106,9 +221,9 @@ func TestTranslator(t *testing.T) {
 					},
 				},
 			},
-			pipelineName:   common.PipelineNameHostCustomMetrics,
-			mode:           config.ModeEC2,
-			runInContainer: true,
+			pipelineName: common.PipelineNameHostCustomMetrics,
+			mode:         config.ModeEC2,
+			isECS:        true,
 			want: &want{
 				pipelineID: "metrics/hostCustomMetrics",
 				receivers:  []string{"nop", "other"},
@@ -215,8 +330,18 @@ func TestTranslator(t *testing.T) {
 	}
 	for name, testCase := range testCases {
 		t.Run(name, func(t *testing.T) {
-			context.CurrentContext().SetMode(testCase.mode)
-			context.CurrentContext().SetRunInContainer(testCase.runInContainer)
+			resetContext()
+			if testCase.mode != "" {
+				context.CurrentContext().SetMode(testCase.mode)
+			}
+			if testCase.kubernetesMode != "" {
+				context.CurrentContext().SetKubernetesMode(testCase.kubernetesMode)
+				context.CurrentContext().SetRunInContainer(true)
+			}
+			if testCase.isECS {
+				ecsutil.GetECSUtilSingleton().Region = "test-region"
+				context.CurrentContext().SetRunInContainer(true)
+			}
 			ht := NewTranslator(
 				testCase.pipelineName,
 				common.NewTranslatorMap[component.Config](
@@ -240,4 +365,10 @@ func TestTranslator(t *testing.T) {
 			}
 		})
 	}
+}
+
+func resetContext() {
+	context.ResetContext()
+	ecsutil.GetECSUtilSingleton().Region = ""
+	context.CurrentContext().SetRunInContainer(false)
 }
