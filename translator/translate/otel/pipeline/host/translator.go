74b183d5b5aae86f43ee9c259c7c5b9002665262
musa-asad <musaasad@amazon.com>
2025-02-02 19:49:08 -0500

    add debug exporter

--- translator/translate/otel/pipeline/host/translator.go
+++ translator/translate/otel/pipeline/host/translator.go
@@ -5,6 +5,7 @@ package host
 
 import (
 	"fmt"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/exporter/debug"
 	"log"
 	"slices"
 	"strings"
@@ -109,6 +110,9 @@ func (t translator) Translate(conf *confmap.Conf) (*common.ComponentTranslators,
 		if currentContext.KubernetesMode() != "" {
 			translators.Processors.Set(k8sattributesprocessor.NewTranslatorWithName(t.name))
 			entityProcessor = awsentity.NewTranslatorWithEntityType(awsentity.Service, common.OtlpKey, false)
+			if enabled, _ := common.GetBool(conf, common.AgentDebugConfigKey); enabled {
+				translators.Exporters.Set(debug.NewTranslator(common.WithName(t.name)))
+			}
 		}
 	case common.PipelineNameHostCustomMetrics:
 		if !currentContext.RunInContainer() {
