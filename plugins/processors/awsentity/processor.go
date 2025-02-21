c23457c5cb3e93800ba92372132e643d57e9ebe4
POOJA REDDY NATHALA <poojardy@amazon.com>
2025-02-21 21:33:32 +0530

    added fix in entity logic to fallback to workload name when unknown service is set by OTEL SDK (#1557)

--- plugins/processors/awsentity/processor.go
+++ plugins/processors/awsentity/processor.go
@@ -27,6 +27,7 @@ const (
 	attributeService                       = "Service"
 	attributeEC2TagAwsAutoscalingGroupName = "ec2.tag.aws:autoscaling:groupName"
 	EMPTY                                  = ""
+	unknownService                         = "unknown_service"
 )
 
 type scraper interface {
@@ -187,13 +188,14 @@ func (p *awsEntityProcessor) processMetrics(_ context.Context, md pmetric.Metric
 				}
 
 				podInfo, ok := p.k8sscraper.(*k8sattributescraper.K8sAttributeScraper)
-				// Perform fallback mechanism for service and environment name if they
-				// are empty
-				if entityServiceName == EMPTY && ok && podInfo != nil && podInfo.Workload != EMPTY {
+				// Perform fallback mechanism for service name if it is empty
+				// or has prefix unknown_service ( unknown_service will be set by OTEL SDK if the service name is empty on application pod)
+				// https://opentelemetry.io/docs/specs/semconv/attributes-registry/service/
+				if (entityServiceName == EMPTY || strings.HasPrefix(entityServiceName, unknownService)) && ok && podInfo != nil && podInfo.Workload != EMPTY {
 					entityServiceName = podInfo.Workload
 					entityServiceNameSource = entitystore.ServiceNameSourceK8sWorkload
 				}
-
+				// Perform fallback mechanism for environment if it is empty
 				if entityEnvironmentName == EMPTY && ok && podInfo.Cluster != EMPTY && podInfo.Namespace != EMPTY {
 					if p.config.KubernetesMode == config.ModeEKS {
 						entityEnvironmentName = "eks:" + p.config.ClusterName + "/" + podInfo.Namespace
