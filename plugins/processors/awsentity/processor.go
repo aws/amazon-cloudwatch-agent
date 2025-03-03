20824f27ccf516affc1d3dbf84eed35d06d9df11
POOJA REDDY NATHALA <poojardy@amazon.com>
2025-03-03 09:44:57 -0500

    Set the service name source to Instrumentation in entity when customer sets the service name OTEL resource attribute (#1565)

--- plugins/processors/awsentity/processor.go
+++ plugins/processors/awsentity/processor.go
@@ -195,6 +195,10 @@ func (p *awsEntityProcessor) processMetrics(_ context.Context, md pmetric.Metric
 					entityServiceName = podInfo.Workload
 					entityServiceNameSource = entitystore.ServiceNameSourceK8sWorkload
 				}
+				// Set the service name source to Instrumentation if the operator doesn't set it
+				if entityServiceName != EMPTY && entityServiceNameSource == EMPTY && getTelemetrySDKEnabledAttribute(resourceAttrs) {
+					entityServiceNameSource = entitystore.ServiceNameSourceInstrumentation
+				}
 				// Perform fallback mechanism for environment if it is empty
 				if entityEnvironmentName == EMPTY && ok && podInfo.Cluster != EMPTY && podInfo.Namespace != EMPTY {
 					if p.config.KubernetesMode == config.ModeEKS {
@@ -491,6 +495,13 @@ func getServiceAttributes(p pcommon.Map) string {
 		return serviceName.Str()
 	}
 	return EMPTY
+}
+
+func getTelemetrySDKEnabledAttribute(p pcommon.Map) bool {
+	if _, ok := p.Get(semconv.AttributeTelemetrySDKName); ok {
+		return true
+	}
+	return false
 }
 
 // scrapeK8sPodName gets the k8s pod name which is full pod name from the resource attributes
