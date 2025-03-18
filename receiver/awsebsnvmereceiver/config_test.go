ef179b4c4f0cdbe9621a531673035c30b95b0382
Dominic Sciascia <dscia@amazon.com>
2025-03-18 20:19:40 +0000

    add resource filtering unit test

--- receiver/awsebsnvmereceiver/config_test.go
+++ receiver/awsebsnvmereceiver/config_test.go
@@ -6,6 +6,7 @@ package awsebsnvmereceiver
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -13,4 +14,38 @@ func TestConfigValidate(t *testing.T) {
 	c := Config{}
 	err := c.Validate()
 	require.NotNil(t, err)
+}
+
+func TestConfigWithResources(t *testing.T) {
+	testCases := []struct {
+		name      string
+		resources []string
+	}{
+		{
+			name:      "empty resources",
+			resources: []string{},
+		},
+		{
+			name:      "single resource",
+			resources: []string{"nvme0n1"},
+		},
+		{
+			name:      "multiple resources",
+			resources: []string{"nvme0n1", "nvme1n1"},
+		},
+		{
+			name:      "wildcard",
+			resources: []string{"*"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := createDefaultConfig().(*Config)
+			cfg.Resources = tc.resources
+
+			// Just verify we can set the resources field
+			assert.Equal(t, tc.resources, cfg.Resources)
+		})
+	}
 }
