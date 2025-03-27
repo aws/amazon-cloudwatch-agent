5194f1745b3cb3779b953a0bf34c682c2bdadde3
Rick Rossi <dricross@amazon.com>
2025-03-27 09:53:14 -0400

    Set default staleness to 2 weeks

--- translator/translate/otel/processor/deltatocumulativeprocessor/translator.go
+++ translator/translate/otel/processor/deltatocumulativeprocessor/translator.go
@@ -4,6 +4,8 @@
 package deltatocumulativeprocessor
 
 import (
+	"time"
+
 	"github.com/open-telemetry/opentelemetry-collector-contrib/processor/deltatocumulativeprocessor"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/confmap"
@@ -37,5 +39,6 @@ func (t *translator) ID() component.ID {
 // Metrics section of the JSON config.
 func (t *translator) Translate(conf *confmap.Conf) (component.Config, error) {
 	cfg := t.factory.CreateDefaultConfig().(*deltatocumulativeprocessor.Config)
+	cfg.MaxStale = 14 * 24 * time.Hour // two weeks
 	return cfg, nil
 }
