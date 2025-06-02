6e3bdcff7a1b82e774d0c064e6d3df8db4cc2ff7
Paramadon <parampreet0623@gmail.com>
2025-06-02 11:28:33 -0400

    Testing framework

--- translator/translate/otel/processor/metricsdecorator/translator_windows_test.go
+++ translator/translate/otel/processor/metricsdecorator/translator_windows_test.go
@@ -68,7 +68,7 @@ func TestMetricDecoration(t *testing.T) {
 	metrics.AddGaugeMetricDataPoint("Connections_Established", "none", 0.0, 0, 0, nil)
 
 	ctx := context.Background()
-	proc, err := transl.factory.CreateMetrics(ctx, processortest.NewNopSettings(component.MustNewType("metricsdecorator")), cfg, sink)
+	proc, err := transl.factory.CreateMetrics(ctx, processortest.NewNopSettings(component.MustNewType("transform")), cfg, sink)
 	require.NotNil(t, proc)
 	require.NoError(t, err)
 	actualMetrics := pmetric.NewMetrics()
