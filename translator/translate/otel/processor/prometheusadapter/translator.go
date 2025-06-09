90fb6c4f87d1d2d2732f71791d3b183dfb201f67
Rick <dricross@amazon.com>
2025-06-09 17:02:18 -0400

    Replace telegraf prometheus plugin (#1709)

---
+++ translator/translate/otel/processor/prometheusadapter/translator.go
@@ -0,0 +1,33 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package prometheusadapter
+
+import (
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/processor"
+
+	"github.com/aws/amazon-cloudwatch-agent/plugins/processors/prometheusadapter"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+)
+
+type translator struct {
+	name    string
+	factory processor.Factory
+}
+
+var _ common.ComponentTranslator = (*translator)(nil)
+
+func NewTranslatorWithName(name string) common.ComponentTranslator {
+	return &translator{name, prometheusadapter.NewFactory()}
+}
+
+func (t *translator) ID() component.ID {
+	return component.NewIDWithName(t.factory.Type(), t.name)
+}
+
+func (t *translator) Translate(conf *confmap.Conf) (component.Config, error) {
+	cfg := t.factory.CreateDefaultConfig().(*prometheusadapter.Config)
+	return cfg, nil
+}
