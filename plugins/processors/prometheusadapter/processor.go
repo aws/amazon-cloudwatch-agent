80d6c53f3047fc9e62e472ad50a25db01d73e876
Rick Rossi <dricross@amazon.com>
2025-06-09 17:18:11 -0400

    update processor to convert metrics to telegraf plugin format

--- plugins/processors/prometheusadapter/processor.go
+++ plugins/processors/prometheusadapter/processor.go
@@ -5,9 +5,13 @@ package prometheusadapter
 
 import (
 	"context"
+	"os"
 
+	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.uber.org/zap"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/translator/prometheus"
 )
 
 type prometheusAdapterProcessor struct {
@@ -24,5 +28,101 @@ func newPrometheusAdapterProcessor(config *Config, logger *zap.Logger) *promethe
 }
 
 func (d *prometheusAdapterProcessor) processMetrics(_ context.Context, md pmetric.Metrics) (pmetric.Metrics, error) {
+	rms := md.ResourceMetrics()
+	for i := 0; i < rms.Len(); i++ {
+		rm := rms.At(i)
+		rma := rm.Resource().Attributes()
+		sms := rm.ScopeMetrics()
+		for j := 0; j < sms.Len(); j++ {
+			metrics := sms.At(j).Metrics()
+
+			for k := 0; k < metrics.Len(); k++ {
+				m := metrics.At(k)
+				d.processMetric(m, rma)
+			}
+
+		}
+
+		// Remove extraneous resource attributes
+		// This must be done after processing metrics so that the resource attributes can be moved to datapoint attributes
+		rma.RemoveIf(func(key string, value pcommon.Value) bool {
+			extraneousAttributes := map[string]struct{}{
+				"http.scheme":         {},
+				"net.host.port":       {},
+				"net.host.name":       {},
+				"server.port":         {},
+				"server.address":      {},
+				"service.instance.id": {},
+				"service.name":        {},
+				"url.scheme":          {},
+			}
+			_, ok := extraneousAttributes[key]
+			return ok
+		})
+	}
 	return md, nil
+}
+
+func (d *prometheusAdapterProcessor) processMetric(m pmetric.Metric, rma pcommon.Map) {
+	switch m.Type() {
+	case pmetric.MetricTypeGauge:
+		processNumberDataPointSlice(m.Gauge().DataPoints(), rma)
+	case pmetric.MetricTypeSum:
+		processNumberDataPointSlice(m.Sum().DataPoints(), rma)
+	case pmetric.MetricTypeSummary:
+		processSummaryDataPointSlice(m.Summary().DataPoints(), rma)
+	case pmetric.MetricTypeHistogram:
+		processHistogramDataPointSlice(m.Histogram().DataPoints(), rma)
+	case pmetric.MetricTypeExponentialHistogram:
+		processExponentialHistogramDataPointSlice(m.ExponentialHistogram().DataPoints(), rma)
+	case pmetric.MetricTypeEmpty:
+		d.logger.Debug("Ignore empty metric")
+	default:
+		d.logger.Debug("Ignore unknown metric type %s", zap.String("type", m.Type().String()))
+	}
+
+	if v, ok := m.Metadata().Get(prometheus.MetricMetadataTypeKey); ok {
+		v.AsString()
+	}
+}
+
+func processNumberDataPointSlice(dps pmetric.NumberDataPointSlice, rma pcommon.Map) {
+	for i := 0; i < dps.Len(); i++ {
+		updateDatapointAttributes(dps.At(i).Attributes(), rma)
+	}
+}
+
+func processSummaryDataPointSlice(dps pmetric.SummaryDataPointSlice, rma pcommon.Map) {
+	for i := 0; i < dps.Len(); i++ {
+		updateDatapointAttributes(dps.At(i).Attributes(), rma)
+	}
+}
+
+func processHistogramDataPointSlice(dps pmetric.HistogramDataPointSlice, rma pcommon.Map) {
+	for i := 0; i < dps.Len(); i++ {
+		updateDatapointAttributes(dps.At(i).Attributes(), rma)
+	}
+}
+
+func processExponentialHistogramDataPointSlice(dps pmetric.ExponentialHistogramDataPointSlice, rma pcommon.Map) {
+	for i := 0; i < dps.Len(); i++ {
+		updateDatapointAttributes(dps.At(i).Attributes(), rma)
+	}
+}
+
+func updateDatapointAttributes(attr pcommon.Map, rma pcommon.Map) {
+	// add new attributes
+	attr.PutStr("receiver", "prometheus")
+	hostname, err := os.Hostname()
+	if err == nil {
+		attr.PutStr("host", hostname)
+	}
+
+	// relabel
+	if serviceName, ok := rma.Get("service.name"); ok {
+		attr.PutStr("job", serviceName.AsString())
+	}
+	if serviceInstanceId, ok := rma.Get("service.instance.id"); ok {
+		attr.PutStr("instance", serviceInstanceId.AsString())
+	}
 }
