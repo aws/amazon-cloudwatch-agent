ae49b002944cdd2303445fcc96e06719c9b27e34
Rick <dricross@amazon.com>
2025-06-09 17:02:18 -0400

    Replace telegraf prometheus plugin (#1709)

---
+++ plugins/processors/prometheusadapter/factory.go
@@ -0,0 +1,56 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package prometheusadapter
+
+import (
+	"context"
+	"fmt"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/consumer"
+	"go.opentelemetry.io/collector/processor"
+	"go.opentelemetry.io/collector/processor/processorhelper"
+)
+
+const (
+	stability = component.StabilityLevelBeta
+)
+
+var (
+	TypeStr, _            = component.NewType("prometheusadapter")
+	processorCapabilities = consumer.Capabilities{MutatesData: true}
+)
+
+func NewFactory() processor.Factory {
+	return processor.NewFactory(
+		TypeStr,
+		createDefaultConfig,
+		processor.WithMetrics(createMetricsProcessor, stability))
+}
+
+func createDefaultConfig() component.Config {
+	return &Config{}
+}
+
+func createMetricsProcessor(
+	ctx context.Context,
+	set processor.Settings,
+	cfg component.Config,
+	nextConsumer consumer.Metrics,
+) (processor.Metrics, error) {
+	processorConfig, ok := cfg.(*Config)
+	if !ok {
+		return nil, fmt.Errorf("configuration parsing error")
+	}
+
+	metricsProcessor := newPrometheusAdapterProcessor(processorConfig, set.Logger)
+
+	return processorhelper.NewMetrics(
+		ctx,
+		set,
+		cfg,
+		nextConsumer,
+		metricsProcessor.processMetrics,
+		processorhelper.WithCapabilities(processorCapabilities))
+}
