3897aecb9dcb81d9d683fde2f159b32c9ae8a5e8
Rick Rossi <dricross@amazon.com>
2025-06-12 08:58:39 -0400

    Set detailed metrics flag

--- translator/translate/otel/exporter/awsemf/translator.go
+++ translator/translate/otel/exporter/awsemf/translator.go
@@ -127,6 +127,7 @@ func (t *translator) Translate(c *confmap.Conf) (component.Config, error) {
 	if context.CurrentContext().Mode() == config.ModeOnPrem || context.CurrentContext().Mode() == config.ModeOnPremise {
 		cfg.AWSSessionSettings.LocalMode = true
 	}
+	cfg.DetailedMetrics = true
 
 	if t.isAppSignals(c) {
 		if err := setAppSignalsFields(c, cfg); err != nil {
