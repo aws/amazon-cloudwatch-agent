b84f421f367707a4a85d430fee4e7d0d7eb23d39
Jeffrey Chien <chienjef@amazon.com>
2025-07-11 12:07:38 -0400

    Add default log publishing concurrency

--- translator/translate/logs/ruleConcurrency.go
+++ translator/translate/logs/ruleConcurrency.go
@@ -3,23 +3,81 @@
 
 package logs
 
-import "github.com/aws/amazon-cloudwatch-agent/translator"
+import (
+	"errors"
+	"runtime"
 
-const ConcurrencySectionKey = "concurrency"
+	"github.com/aws/amazon-cloudwatch-agent/translator"
+	"github.com/aws/amazon-cloudwatch-agent/translator/context"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/logs/constants"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/util"
+)
+
+const (
+	ConcurrencySectionKey = "concurrency"
+)
+
+var (
+	logFileCollectListPath      = util.Path(constants.SectionKeyLogsCollected, constants.SectionKeyFiles, constants.SectionKeyCollectList)
+	checkTimestampFormatVisitor = util.NewSliceVisitor(util.NewVisitor(isMissingTimestampFormat))
+	// based on AWS Java SDK default
+	defaultConcurrency = max(runtime.NumCPU(), 8)
+)
 
 type Concurrency struct {
 }
 
 func (c *Concurrency) ApplyRule(input any) (string, any) {
-	result := map[string]interface{}{}
-	_, val := translator.DefaultCase(ConcurrencySectionKey, float64(0), input)
+	result := map[string]any{}
+	_, val := translator.DefaultCase(ConcurrencySectionKey, nil, input)
 	var concurrency int
-	if v, ok := val.(float64); ok && v > 1 {
+	if v, ok := val.(float64); ok {
 		concurrency = int(v)
-		result[ConcurrencySectionKey] = concurrency
+	} else {
+		concurrency = determineDefault(input)
 	}
-	GlobalLogConfig.Concurrency = concurrency
+	if concurrency > 1 {
+		result[ConcurrencySectionKey] = concurrency
+		GlobalLogConfig.Concurrency = concurrency
+	} else {
+		GlobalLogConfig.Concurrency = -1
+	}
 	return Output_Cloudwatch_Logs, result
+}
+
+// determineDefault determines the default concurrency if not set. Will not set a default if timestamp_format is
+// missing in the configuration for the files being collected.
+func determineDefault(input any) int {
+	m, ok := input.(map[string]any)
+	if !ok {
+		return -1
+	}
+	_, ok = m[constants.SectionKeyLogsCollected]
+	if !ok || isMissingAnyTimestampFormat(input, logFileCollectListPath) {
+		return -1
+	}
+	return defaultConcurrency
+}
+
+func isMissingAnyTimestampFormat(input any, path string) bool {
+	return errors.Is(util.Visit(input, path, checkTimestampFormatVisitor), util.ErrTargetNotFound)
+}
+
+func isMissingTimestampFormat(input any) error {
+	m, ok := input.(map[string]any)
+	if !ok {
+		return util.ErrTargetNotFound
+	}
+	filePath, ok := m[constants.SectionKeyFilePath]
+	// skip the agent log file if configured as timestamp format is not supported https://github.com/aws/amazon-cloudwatch-agent/pull/885
+	if ok && filePath == context.CurrentContext().GetAgentLogFile() {
+		return nil
+	}
+	_, ok = m[constants.SectionKeyTimestampFormat]
+	if !ok {
+		return util.ErrTargetNotFound
+	}
+	return nil
 }
 
 func init() {
