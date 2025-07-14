94cd061138c79ea3322aa9872a978e2149e5a558
Dominic Sciascia <duhminick@users.noreply.github.com>
2025-07-14 14:41:12 -0400

    Restore and send gaps for Windows events (#1747)
    Co-authored-by: Jeffrey Chien <chienjef@amazon.com>

--- plugins/inputs/windows_event_log/wineventlog/windows_event_api.go
+++ plugins/inputs/windows_event_log/wineventlog/windows_event_api.go
@@ -10,6 +10,19 @@ import (
 	"unsafe"
 )
 
+// WindowsEventAPI defines the interface for Windows Event Log API operations
+// This allows us to mock the Windows API calls for testing
+type WindowsEventAPI interface {
+	EvtSubscribe(session EvtHandle, signalEvent uintptr, channelPath *uint16, query *uint16, bookmark EvtHandle, context uintptr, callback syscall.Handle, flags EvtSubscribeFlag) (handle EvtHandle, err error)
+	EvtQuery(session EvtHandle, path *uint16, query *uint16, flags EvtQueryFlag) (EvtHandle, error)
+	EvtNext(resultSet EvtHandle, eventArraySize uint32, eventArray *EvtHandle, timeout uint32, flags uint32, numReturned *uint32) error
+	EvtClose(handle EvtHandle) error
+	EvtRender(context EvtHandle, fragment EvtHandle, flags EvtRenderFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32, propertyCount *uint32) error
+	EvtCreateBookmark(bookmarkXML *uint16) (handle EvtHandle, err error)
+	EvtFormatMessage(publisherMetadata EvtHandle, event EvtHandle, messageID uint32, valueCount uint32, values uintptr, flags EvtFormatMessageFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32) error
+	EvtOpenPublisherMetadata(session EvtHandle, publisherId *uint16, logFilePath *uint16, locale uint32, flags uint32) (EvtHandle, error)
+}
+
 // EvtHandle is a handle to the event log.
 type EvtHandle uintptr
 
@@ -18,6 +31,14 @@ type EvtSubscribeFlag uint32
 
 const (
 	EvtSubscribeStartAfterBookmark EvtSubscribeFlag = 3
+)
+
+// EvtQueryFlag defines the values that specify how to return the query results and whether you are query against a channel or log file.
+// https://learn.microsoft.com/en-us/windows/win32/api/winevt/nf-winevt-evtquery
+type EvtQueryFlag uint32
+
+const (
+	EvtQueryChannelPath EvtQueryFlag = 1
 )
 
 // EvtRenderFlag defines the values that specify what to render.
@@ -45,6 +66,7 @@ var (
 	// For Windows versions newer than 2003
 	modwevtapi                   = syscall.NewLazyDLL("wevtapi.dll")
 	procEvtSubscribe             = modwevtapi.NewProc("EvtSubscribe")
+	procEvtQuery                 = modwevtapi.NewProc("EvtQuery")
 	procEvtCreateBookmark        = modwevtapi.NewProc("EvtCreateBookmark")
 	procEvtCreateRenderContext   = modwevtapi.NewProc("EvtCreateRenderContext")
 	procEvtRender                = modwevtapi.NewProc("EvtRender")
@@ -54,7 +76,14 @@ var (
 	procEvtOpenPublisherMetadata = modwevtapi.NewProc("EvtOpenPublisherMetadata")
 )
 
-func EvtSubscribe(session EvtHandle, signalEvent uintptr, channelPath *uint16, query *uint16, bookmark EvtHandle, context uintptr, callback syscall.Handle, flags EvtSubscribeFlag) (handle EvtHandle, err error) {
+// windowsEventAPI implements the interface using actual Windows API calls
+type windowsEventAPI struct{}
+
+func NewWindowsEventAPI() WindowsEventAPI {
+	return &windowsEventAPI{}
+}
+
+func (w *windowsEventAPI) EvtSubscribe(session EvtHandle, signalEvent uintptr, channelPath *uint16, query *uint16, bookmark EvtHandle, context uintptr, callback syscall.Handle, flags EvtSubscribeFlag) (handle EvtHandle, err error) {
 	r0, _, e1 := syscall.Syscall9(procEvtSubscribe.Addr(), 8, uintptr(session), uintptr(signalEvent), uintptr(unsafe.Pointer(channelPath)), uintptr(unsafe.Pointer(query)), uintptr(bookmark), uintptr(context), uintptr(callback), uintptr(flags), 0)
 	handle = EvtHandle(r0)
 	if handle == 0 {
@@ -67,7 +96,20 @@ func EvtSubscribe(session EvtHandle, signalEvent uintptr, channelPath *uint16, q
 	return
 }
 
-func EvtCreateBookmark(bookmarkXML *uint16) (handle EvtHandle, err error) {
+func (w *windowsEventAPI) EvtQuery(session EvtHandle, path *uint16, query *uint16, flags EvtQueryFlag) (handle EvtHandle, err error) {
+	r0, _, e1 := syscall.Syscall6(procEvtQuery.Addr(), 4, uintptr(session), uintptr(unsafe.Pointer(path)), uintptr(unsafe.Pointer(query)), uintptr(flags), 0, 0)
+	handle = EvtHandle(r0)
+	if handle == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+func (w *windowsEventAPI) EvtCreateBookmark(bookmarkXML *uint16) (handle EvtHandle, err error) {
 	r0, _, e1 := syscall.Syscall(procEvtCreateBookmark.Addr(), 1, uintptr(unsafe.Pointer(bookmarkXML)), 0, 0)
 	handle = EvtHandle(r0)
 	if handle == 0 {
@@ -80,7 +122,7 @@ func EvtCreateBookmark(bookmarkXML *uint16) (handle EvtHandle, err error) {
 	return
 }
 
-func EvtCreateRenderContext(ValuePathsCount uint32, valuePaths uintptr, flags EvtRenderContextFlag) (handle EvtHandle, err error) {
+func (w *windowsEventAPI) EvtCreateRenderContext(ValuePathsCount uint32, valuePaths uintptr, flags EvtRenderContextFlag) (handle EvtHandle, err error) {
 	r0, _, e1 := syscall.Syscall(procEvtCreateRenderContext.Addr(), 3, uintptr(ValuePathsCount), uintptr(valuePaths), uintptr(flags))
 	handle = EvtHandle(r0)
 	if handle == 0 {
@@ -93,7 +135,7 @@ func EvtCreateRenderContext(ValuePathsCount uint32, valuePaths uintptr, flags Ev
 	return
 }
 
-func EvtRender(context EvtHandle, fragment EvtHandle, flags EvtRenderFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32, propertyCount *uint32) (err error) {
+func (w *windowsEventAPI) EvtRender(context EvtHandle, fragment EvtHandle, flags EvtRenderFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32, propertyCount *uint32) (err error) {
 	r1, _, e1 := syscall.Syscall9(procEvtRender.Addr(), 7, uintptr(context), uintptr(fragment), uintptr(flags), uintptr(bufferSize), uintptr(unsafe.Pointer(buffer)), uintptr(unsafe.Pointer(bufferUsed)), uintptr(unsafe.Pointer(propertyCount)), 0, 0)
 	if r1 == 0 {
 		if e1 != 0 {
@@ -105,7 +147,7 @@ func EvtRender(context EvtHandle, fragment EvtHandle, flags EvtRenderFlag, buffe
 	return
 }
 
-func EvtClose(object EvtHandle) (err error) {
+func (w *windowsEventAPI) EvtClose(object EvtHandle) (err error) {
 	r1, _, e1 := syscall.Syscall(procEvtClose.Addr(), 1, uintptr(object), 0, 0)
 	if r1 == 0 {
 		if e1 != 0 {
@@ -117,7 +159,7 @@ func EvtClose(object EvtHandle) (err error) {
 	return
 }
 
-func EvtNext(resultSet EvtHandle, eventArraySize uint32, eventArray *EvtHandle, timeout uint32, flags uint32, numReturned *uint32) (err error) {
+func (w *windowsEventAPI) EvtNext(resultSet EvtHandle, eventArraySize uint32, eventArray *EvtHandle, timeout uint32, flags uint32, numReturned *uint32) (err error) {
 	r1, _, e1 := syscall.Syscall6(procEvtNext.Addr(), 6, uintptr(resultSet), uintptr(eventArraySize), uintptr(unsafe.Pointer(eventArray)), uintptr(timeout), uintptr(flags), uintptr(unsafe.Pointer(numReturned)))
 	if r1 == 0 {
 		if e1 != 0 {
@@ -129,7 +171,7 @@ func EvtNext(resultSet EvtHandle, eventArraySize uint32, eventArray *EvtHandle, 
 	return
 }
 
-func EvtFormatMessage(publisherMetadata EvtHandle, event EvtHandle, messageID uint32, valueCount uint32, values uintptr, flags EvtFormatMessageFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32) (err error) {
+func (w *windowsEventAPI) EvtFormatMessage(publisherMetadata EvtHandle, event EvtHandle, messageID uint32, valueCount uint32, values uintptr, flags EvtFormatMessageFlag, bufferSize uint32, buffer *byte, bufferUsed *uint32) (err error) {
 	r1, _, e1 := syscall.Syscall9(procEvtFormatMessage.Addr(), 9, uintptr(publisherMetadata), uintptr(event), uintptr(messageID), uintptr(valueCount), uintptr(values), uintptr(flags), uintptr(bufferSize), uintptr(unsafe.Pointer(buffer)), uintptr(unsafe.Pointer(bufferUsed)))
 	if r1 == 0 {
 		if e1 != 0 {
@@ -141,7 +183,7 @@ func EvtFormatMessage(publisherMetadata EvtHandle, event EvtHandle, messageID ui
 	return
 }
 
-func EvtOpenPublisherMetadata(session EvtHandle, publisherIdentity *uint16, logFilePath *uint16, locale uint32, flags uint32) (handle EvtHandle, err error) {
+func (w *windowsEventAPI) EvtOpenPublisherMetadata(session EvtHandle, publisherIdentity *uint16, logFilePath *uint16, locale uint32, flags uint32) (handle EvtHandle, err error) {
 	r0, _, e1 := syscall.Syscall6(procEvtOpenPublisherMetadata.Addr(), 5, uintptr(session), uintptr(unsafe.Pointer(publisherIdentity)), uintptr(unsafe.Pointer(logFilePath)), uintptr(locale), uintptr(flags), 0)
 	handle = EvtHandle(r0)
 	if handle == 0 {
