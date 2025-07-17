4c87ab3b4aa7fa085231ccceda72fccc58c3ac7f
Parampreet Singh <50599809+Paramadon@users.noreply.github.com>
2025-07-17 18:36:45 -0400

    Fixing prometheus env configuration expansion problem (#1768)

--- translator/tocwconfig/tocwconfig_test.go
+++ translator/tocwconfig/tocwconfig_test.go
@@ -54,6 +54,9 @@ var prometheusConfig string
 //go:embed sampleConfig/prometheus_cwa_config.yaml
 var prometheusPMDConfig string
 
+//go:embed sampleConfig/prometheus_expansion.yaml
+var prometheusExpansionConfig string
+
 type testCase struct {
 	filename        string
 	targetPlatform  string
@@ -460,6 +463,25 @@ func TestPrometheusPMDConfig(t *testing.T) {
 
 	expectedEnvVars := map[string]string{}
 	checkTranslation(t, "prometheus_pmd_config", "linux", expectedEnvVars, "", tokenReplacements)
+}
+
+func TestPrometheusExpansionConfig(t *testing.T) {
+	resetContext(t)
+	context.CurrentContext().SetRunInContainer(true)
+	context.CurrentContext().SetMode(config.ModeEC2)
+	t.Setenv(config.HOST_NAME, "host_name_from_env")
+
+	temp := t.TempDir()
+	prometheusConfigFileName := filepath.Join(temp, "prometheus.yaml")
+	err := os.WriteFile(prometheusConfigFileName, []byte(prometheusExpansionConfig), 0600)
+	require.NoError(t, err)
+
+	tokenReplacements := map[string]string{
+		prometheusFileNameToken: strings.ReplaceAll(prometheusConfigFileName, "\\", "\\\\"),
+	}
+
+	expectedEnvVars := map[string]string{}
+	checkTranslation(t, "prometheus_expansion_config", "linux", expectedEnvVars, "", tokenReplacements)
 }
 
 func TestPrometheusConfigwithTargetAllocator(t *testing.T) {
@@ -924,7 +946,7 @@ func verifyToTomlTranslation(t *testing.T, input interface{}, desiredTomlPath st
 	_, decodeError2 := toml.Decode(tomlStr, &actual)
 	assert.NoError(t, decodeError2)
 
-	// assert.NoError(t, os.WriteFile(desiredTomlPath, []byte(tomlStr), 0644)) // useful for regenerating TOML
+	//assert.NoError(t, os.WriteFile(desiredTomlPath, []byte(tomlStr), 0644)) // useful for regenerating TOML
 
 	// This less function sort the content of string slice in alphabetical order so the
 	// cmp.Equal method will compare the two struct with slices in them, regardless the elements within the slices
