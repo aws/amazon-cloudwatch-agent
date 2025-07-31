84807cd5bfa602f25a7d10ac440c10bacd110702
Akansha Agarwal <agarakan@users.noreply.github.com>
2025-07-31 14:55:06 +0000

    Add jobName in dockerLabel config

--- translator/translate/otel/extension/ecsobserver/translator.go
+++ translator/translate/otel/extension/ecsobserver/translator.go
@@ -90,6 +90,10 @@ func (t *translator) Translate(conf *confmap.Conf) (component.Config, error) {
 	// Docker label based service discovery
 	if dockerLabel, ok := ecsSD["docker_label"].(map[string]interface{}); ok {
 		dockerConfig := ecsobserver.DockerLabelConfig{
+			CommonExporterConfig: ecsobserver.CommonExporterConfig{
+				JobName:     getString(tdMap, "sd_job_name"),
+				MetricsPath: getStringWithDefault(tdMap, "sd_metrics_path", defaultMetricsPath),
+			},
 			MetricsPathLabel: getStringWithDefault(dockerLabel, "sd_metrics_path_label", defaultMetricsPathLabel),
 			PortLabel:        getStringWithDefault(dockerLabel, "sd_port_label", defaultPortLabel),
 			JobNameLabel:     getString(dockerLabel, "sd_job_name_label"),
