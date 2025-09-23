83f0c4a550c5db697afa8b65edfeeaf6f78e8841
Parampreet Singh <50599809+Paramadon@users.noreply.github.com>
2025-09-23 11:44:47 -0400

    Ensure use_dualstack_endpoint rule is applied first  (#1874)

--- translator/processRuleToApply.go
+++ translator/processRuleToApply.go
@@ -6,7 +6,19 @@ package translator
 // ProcessRuleToApply check if the specification configuration provide some configs that need translation with some rules
 // In json the default configuration should be like "cpu":{"per_cpu":false}
 func ProcessRuleToApply(input interface{}, childRule map[string]Rule, result map[string]interface{}) map[string]interface{} {
-	for _, rule := range childRule {
+	// Process use_dualstack_endpoint rule first if it exists to ensure dualstack endpoint is set
+	if rule, exists := childRule["use_dualstack_endpoint"]; exists {
+		key, val := rule.ApplyRule(input)
+		if key != "" {
+			result[key] = val
+		}
+	}
+
+	for ruleKey, rule := range childRule {
+		// Skip use_dualstack_endpoint as it has already been set
+		if ruleKey == "use_dualstack_endpoint" {
+			continue
+		}
 		key, val := rule.ApplyRule(input)
 		if key != "" {
 			result[key] = val
