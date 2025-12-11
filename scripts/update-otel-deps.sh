b8ad937f4ed2b4a4142c595f45725f6313bf437e
Marcus Mann <mpmann@amazon.com>
2025-12-11 13:44:38 -0500

    Fix script to only update replace directives and use correct format
    - Only match replace directives with '=>' prefix to avoid updating require section
    - Use space-separated format instead of @version for go mod compatibility
    - Let go mod tidy generate proper pseudo-versions

--- scripts/update-otel-deps.sh
+++ scripts/update-otel-deps.sh
@@ -2,12 +2,11 @@
 
 # Get latest commit SHA from aws-cwa-dev branch
 LATEST_SHA=$(git ls-remote https://github.com/amazon-contributing/opentelemetry-collector-contrib.git aws-cwa-dev | awk '{print $1}')
-TIMESTAMP=$(date -u +%Y%m%d%H%M%S)
 
 echo "Updating to commit: $LATEST_SHA"
 
-# Update only amazon-contributing references to use the new SHA
-sed -i '' "s|github\.com/amazon-contributing/opentelemetry-collector-contrib/\([^@]*\) v0\.0\.0-[0-9]*-[a-f0-9]*|github.com/amazon-contributing/opentelemetry-collector-contrib/\1 v0.0.0-$TIMESTAMP-$LATEST_SHA|g" go.mod
+# Update only amazon-contributing references in replace directives
+sed -i '' "s|=> github\.com/amazon-contributing/opentelemetry-collector-contrib/\([^@]*\) v0\.0\.0-[0-9]*-[a-f0-9]*|=> github.com/amazon-contributing/opentelemetry-collector-contrib/\1 $LATEST_SHA|g" go.mod
 
 go mod tidy
 
