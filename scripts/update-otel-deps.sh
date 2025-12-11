1e5bdf5406c7cc50e937fde93577685065d973d6
Marcus Mann <mpmann@amazon.com>
2025-12-11 13:34:50 -0500

    Remove backup file creation from update script

--- scripts/update-otel-deps.sh
+++ scripts/update-otel-deps.sh
@@ -7,7 +7,7 @@ TIMESTAMP=$(date -u +%Y%m%d%H%M%S)
 echo "Updating to commit: $LATEST_SHA"
 
 # Update only amazon-contributing references to use the new SHA
-sed -i.bak "s|github\.com/amazon-contributing/opentelemetry-collector-contrib/\([^@]*\) v0\.0\.0-[0-9]*-[a-f0-9]*|github.com/amazon-contributing/opentelemetry-collector-contrib/\1 v0.0.0-$TIMESTAMP-$LATEST_SHA|g" go.mod
+sed -i '' "s|github\.com/amazon-contributing/opentelemetry-collector-contrib/\([^@]*\) v0\.0\.0-[0-9]*-[a-f0-9]*|github.com/amazon-contributing/opentelemetry-collector-contrib/\1 v0.0.0-$TIMESTAMP-$LATEST_SHA|g" go.mod
 
 go mod tidy
 
