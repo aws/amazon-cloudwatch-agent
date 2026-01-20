0aa65b69b036fff5b4e13519b87d0c0548720d11
Akansha Agarwal <agarakan@users.noreply.github.com>
2026-01-20 20:50:25 +0000

    Lint

--- plugins/outputs/cloudwatchlogs/internal/pusher/pusher.go
+++ plugins/outputs/cloudwatchlogs/internal/pusher/pusher.go
@@ -34,7 +34,7 @@ func NewPusher(
 	flushTimeout time.Duration,
 	retryDuration time.Duration,
 	wg *sync.WaitGroup,
-	concurrency int,
+	_ int,
 	retryHeap RetryHeap,
 ) *Pusher {
 	s := createSender(logger, service, targetManager, workerPool, retryDuration, retryHeap)
