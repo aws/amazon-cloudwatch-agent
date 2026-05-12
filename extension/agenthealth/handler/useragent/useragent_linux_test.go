40f15a8c7a872190653f82348f315ae9032566e0
Michael Commey <mcommey@amazon.com>
2026-05-12 20:15:24 +0000

    Add jd_enabled feature flag for journald adoption metrics

--- extension/agenthealth/handler/useragent/useragent_linux_test.go
+++ extension/agenthealth/handler/useragent/useragent_linux_test.go
@@ -8,21 +8,16 @@ package useragent
 import (
 	"testing"
 
-	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/input/journald"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/journaldreceiver"
 	"github.com/stretchr/testify/assert"
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/otelcol"
-	"go.opentelemetry.io/collector/pipeline"
-	"go.opentelemetry.io/collector/service"
-	"go.opentelemetry.io/collector/service/pipelines"
 )
 
 func TestSetJournaldFeatureFlags(t *testing.T) {
 	tests := []struct {
 		name          string
 		receivers     map[component.ID]component.Config
-		pipelines     pipelines.Config
 		expectedFlags []string
 	}{
 		{
@@ -31,91 +26,11 @@ func TestSetJournaldFeatureFlags(t *testing.T) {
 			expectedFlags: []string{},
 		},
 		{
-			name: "no features",
+			name: "journald enabled",
 			receivers: map[component.ID]component.Config{
 				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{},
 			},
-			expectedFlags: []string{},
-		},
-		{
-			name: "default priority not flagged",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{
-					InputConfig: journald.Config{
-						Priority: "info",
-					},
-				},
-			},
-			expectedFlags: []string{},
-		},
-		{
-			name: "jd_units",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{
-					InputConfig: journald.Config{
-						Units: []string{"sshd", "kubelet"},
-					},
-				},
-			},
-			expectedFlags: []string{flagJournaldUnits},
-		},
-		{
-			name: "jd_priority",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{
-					InputConfig: journald.Config{
-						Priority: "warning",
-					},
-				},
-			},
-			expectedFlags: []string{flagJournaldPriority},
-		},
-		{
-			name: "jd_matches",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{
-					InputConfig: journald.Config{
-						Matches: []journald.MatchConfig{{"_SYSTEMD_UNIT": "ssh.service"}},
-					},
-				},
-			},
-			expectedFlags: []string{flagJournaldMatches},
-		},
-		{
-			name: "jd_filters",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{},
-			},
-			pipelines: pipelines.Config{
-				pipeline.NewIDWithName(pipeline.SignalLogs, "journald/0"): {
-					Processors: []component.ID{
-						component.NewIDWithName(component.MustNewType("filter"), "journald_0"),
-						component.NewIDWithName(component.MustNewType("batch"), "journald_0"),
-					},
-				},
-			},
-			expectedFlags: []string{flagJournaldFilters},
-		},
-		{
-			name: "all flags",
-			receivers: map[component.ID]component.Config{
-				component.MustNewID("journald"): &journaldreceiver.JournaldConfig{
-					InputConfig: journald.Config{
-						Units:    []string{"sshd"},
-						Priority: "err",
-						Matches:  []journald.MatchConfig{{"_UID": "0"}},
-					},
-				},
-			},
-			pipelines: pipelines.Config{
-				pipeline.NewIDWithName(pipeline.SignalLogs, "journald/0"): {
-					Processors: []component.ID{
-						component.NewIDWithName(component.MustNewType("filter"), "journald_0"),
-						component.NewIDWithName(component.MustNewType("batch"), "journald_0"),
-					},
-				},
-			},
-			expectedFlags: []string{flagJournaldUnits, flagJournaldPriority, flagJournaldMatches, flagJournaldFilters},
+			expectedFlags: []string{flagJournaldEnabled},
 		},
 	}
 
@@ -124,9 +39,6 @@ func TestSetJournaldFeatureFlags(t *testing.T) {
 			ua := newUserAgent()
 			otelCfg := &otelcol.Config{
 				Receivers: tt.receivers,
-				Service: service.Config{
-					Pipelines: tt.pipelines,
-				},
 			}
 
 			ua.setJournaldFeatureFlags(otelCfg)
