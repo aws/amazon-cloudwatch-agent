922552e861b3eab0646353a39db211752f021024
Jason Polanco <polajaso@amazon.com>
2026-06-01 16:35:04 -0400

    Add pipeline translator for database monitoring (#2138)

---
+++ translator/translate/otel/pipeline/opentelemetry/databaseinsights/translator.go
@@ -0,0 +1,158 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package databaseinsights
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/pipeline"
+
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/connector/count"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/connector/forward"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/connector/signaltometrics"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/processor/filterprocessor"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/processor/resourcedetection"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/processor/transformprocessor"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/receiver/filelog"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/receiver/postgresql"
+)
+
+type dbiPipelineType int
+
+const (
+	dbiMetrics dbiPipelineType = iota
+	dbiLogToMetrics
+	dbiRawEvents
+	dbiServerLogs
+)
+
+type dbiTranslator struct {
+	pipelineType  dbiPipelineType
+	instanceIndex int
+	cfg           dbiInstanceConfig
+}
+
+var _ common.PipelineTranslator = (*dbiTranslator)(nil)
+
+func (t *dbiTranslator) ID() pipeline.ID {
+	idx := strconv.Itoa(t.instanceIndex)
+	switch t.pipelineType {
+	case dbiMetrics:
+		return pipeline.NewIDWithName(pipeline.SignalMetrics, "dbi_postgresql_"+idx)
+	case dbiLogToMetrics:
+		return pipeline.NewIDWithName(pipeline.SignalLogs, "dbi_postgresql_"+idx)
+	case dbiRawEvents:
+		return pipeline.NewIDWithName(pipeline.SignalLogs, "dbi_postgresql_rawevents_"+idx)
+	case dbiServerLogs:
+		return pipeline.NewIDWithName(pipeline.SignalLogs, "dbi_postgresql_serverlogs_"+idx)
+	}
+	return pipeline.NewID(pipeline.SignalMetrics)
+}
+
+func (t *dbiTranslator) Translate(_ *confmap.Conf) (*common.ComponentTranslators, error) {
+	switch t.pipelineType {
+	case dbiMetrics:
+		return t.translateMetrics()
+	case dbiLogToMetrics:
+		return t.translateLogToMetrics()
+	case dbiRawEvents:
+		return t.translateRawEvents()
+	case dbiServerLogs:
+		return t.translateServerLogs()
+	}
+	return nil, fmt.Errorf("unknown DBI pipeline type: %d", t.pipelineType)
+}
+
+func (t *dbiTranslator) translateMetrics() (*common.ComponentTranslators, error) {
+	idx := strconv.Itoa(t.instanceIndex)
+	fwd := forward.NewTranslator(common.OpenTelemetryKey)
+	countConn := count.NewTranslator(common.DbiConnectorDbload)
+	s2mConn := signaltometrics.NewTranslator(common.DbiConnectorTopsql)
+
+	return &common.ComponentTranslators{
+		Receivers:  common.NewTranslatorMap[component.Config, component.ID](t.pgReceiver("metrics"), countConn, s2mConn),
+		Processors: common.NewTranslatorMap[component.Config, component.ID](transformprocessor.NewTranslatorWithName(common.DbiTransformResource+"_"+idx, transformprocessor.WithMetricStatements(t.resourceStatements())), transformprocessor.NewTranslatorWithName(common.DbiTransformFixStartTime)),
+		Exporters:  common.NewTranslatorMap[component.Config, component.ID](fwd),
+		Extensions: common.NewTranslatorMap[component.Config, component.ID](),
+		Connectors: common.NewTranslatorMap[component.Config, component.ID](fwd, countConn, s2mConn),
+	}, nil
+}
+
+func (t *dbiTranslator) translateLogToMetrics() (*common.ComponentTranslators, error) {
+	countConn := count.NewTranslator(common.DbiConnectorDbload)
+	s2mConn := signaltometrics.NewTranslator(common.DbiConnectorTopsql)
+
+	return &common.ComponentTranslators{
+		Receivers:  common.NewTranslatorMap[component.Config, component.ID](t.pgReceiver("metrics")),
+		Processors: common.NewTranslatorMap[component.Config, component.ID](t.excludeMonitorFilter()),
+		Exporters:  common.NewTranslatorMap[component.Config, component.ID](countConn, s2mConn),
+		Extensions: common.NewTranslatorMap[component.Config, component.ID](),
+		Connectors: common.NewTranslatorMap[component.Config, component.ID](countConn, s2mConn),
+	}, nil
+}
+
+func (t *dbiTranslator) translateRawEvents() (*common.ComponentTranslators, error) {
+	idx := strconv.Itoa(t.instanceIndex)
+	fwd := forward.NewTranslator(common.OpenTelemetryKey)
+
+	return &common.ComponentTranslators{
+		Receivers:  common.NewTranslatorMap[component.Config, component.ID](t.pgReceiver("events", postgresql.WithQuerySampleInterval(60*time.Second))),
+		Processors: common.NewTranslatorMap[component.Config, component.ID](t.excludeMonitorFilter(), resourcedetection.NewTranslator(), transformprocessor.NewTranslatorWithName(common.DbiTransformResource+"_"+idx, transformprocessor.WithMetricStatements(t.resourceStatements()), transformprocessor.WithLogStatements(t.resourceStatements())), transformprocessor.NewTranslatorWithName(common.DbiTransformLogs+"_raw-events_"+idx, transformprocessor.WithLogStatements(t.logStatements("raw-events")))),
+		Exporters:  common.NewTranslatorMap[component.Config, component.ID](fwd),
+		Extensions: common.NewTranslatorMap[component.Config, component.ID](),
+		Connectors: common.NewTranslatorMap[component.Config, component.ID](fwd),
+	}, nil
+}
+
+func (t *dbiTranslator) translateServerLogs() (*common.ComponentTranslators, error) {
+	idx := strconv.Itoa(t.instanceIndex)
+	fwd := forward.NewTranslator(common.OpenTelemetryKey)
+
+	return &common.ComponentTranslators{
+		Receivers:  common.NewTranslatorMap[component.Config, component.ID](filelog.NewTranslator(filelog.WithNamePrefix("postgresql"), filelog.WithIndex(t.instanceIndex), filelog.WithFilePath(t.cfg.logFilePath))),
+		Processors: common.NewTranslatorMap[component.Config, component.ID](resourcedetection.NewTranslator(), transformprocessor.NewTranslatorWithName(common.DbiTransformResource+"_"+idx, transformprocessor.WithMetricStatements(t.resourceStatements()), transformprocessor.WithLogStatements(t.resourceStatements())), transformprocessor.NewTranslatorWithName(common.DbiTransformLogs+"_server-logs_"+idx, transformprocessor.WithLogStatements(t.logStatements("server-logs")))),
+		Exporters:  common.NewTranslatorMap[component.Config, component.ID](fwd),
+		Extensions: common.NewTranslatorMap[component.Config, component.ID](),
+		Connectors: common.NewTranslatorMap[component.Config, component.ID](fwd),
+	}, nil
+}
+
+func (t *dbiTranslator) pgReceiver(name string, extraOpts ...postgresql.Option) common.ComponentTranslator {
+	opts := []postgresql.Option{
+		postgresql.WithName(name),
+		postgresql.WithIndex(t.instanceIndex),
+		postgresql.WithEndpoint(t.cfg.endpoint),
+		postgresql.WithUsername(t.cfg.username),
+		postgresql.WithPassfile(t.cfg.passfile),
+		postgresql.WithCAFile(t.cfg.caFile),
+		postgresql.WithIsLocalhost(t.cfg.isLocalhost),
+	}
+	opts = append(opts, extraOpts...)
+	return postgresql.NewTranslator(opts...)
+}
+
+func (t *dbiTranslator) excludeMonitorFilter() common.ComponentTranslator {
+	idx := strconv.Itoa(t.instanceIndex)
+	condition := fmt.Sprintf(`attributes["user.name"] == "%s" or attributes["postgresql.rolname"] == "%s"`, t.cfg.username, t.cfg.username)
+	return filterprocessor.NewTranslatorWithLogCondition(common.DbiFilterExcludeMonitor+"_"+idx, condition)
+}
+
+func (t *dbiTranslator) resourceStatements() []string {
+	return []string{
+		`set(resource.attributes["db.system.name"], "postgresql")`,
+		fmt.Sprintf(`set(resource.attributes["db.instance.name"], "%s")`, t.cfg.instanceName),
+	}
+}
+
+func (t *dbiTranslator) logStatements(destination string) []string {
+	return []string{
+		fmt.Sprintf(`set(resource.attributes["aws.log.group.name"], "/aws/self-managed-database-insights/postgresql/%s")`, destination),
+		fmt.Sprintf(`set(resource.attributes["aws.log.stream.name"], Concat([resource.attributes["host.id"], "%s"], "/"))`, t.cfg.instanceName),
+	}
+}
---
+++ translator/translate/otel/pipeline/opentelemetry/databaseinsights/translators.go
@@ -0,0 +1,85 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package databaseinsights
+
+import (
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/pipeline"
+
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+)
+
+var (
+	dbiConfigKey = common.ConfigKey(common.OpenTelemetryKey, common.CollectKey, common.DatabaseInsightsKey)
+	dbiPgKey     = common.ConfigKey(common.OpenTelemetryKey, common.CollectKey, common.DatabaseInsightsKey, common.PostgreSQLKey)
+)
+
+type dbiInstanceConfig struct {
+	endpoint     string
+	username     string
+	passfile     string
+	caFile       string
+	instanceName string
+	logFilePath  string
+	isLocalhost  bool
+}
+
+func NewTranslators(conf *confmap.Conf) common.PipelineTranslatorMap {
+	translators := common.NewTranslatorMap[*common.ComponentTranslators, pipeline.ID]()
+	if conf == nil || !conf.IsSet(dbiConfigKey) {
+		return translators
+	}
+
+	instances := parseDbiPostgresqlInstances(conf)
+	for i, cfg := range instances {
+		translators.Set(&dbiTranslator{pipelineType: dbiMetrics, instanceIndex: i, cfg: cfg})
+		translators.Set(&dbiTranslator{pipelineType: dbiLogToMetrics, instanceIndex: i, cfg: cfg})
+		translators.Set(&dbiTranslator{pipelineType: dbiRawEvents, instanceIndex: i, cfg: cfg})
+		if cfg.logFilePath != "" {
+			translators.Set(&dbiTranslator{pipelineType: dbiServerLogs, instanceIndex: i, cfg: cfg})
+		}
+	}
+	return translators
+}
+
+type pgRawInstance struct {
+	Endpoint     string `mapstructure:"endpoint"`
+	Username     string `mapstructure:"username"`
+	PasswordFile string `mapstructure:"password_file"`
+	CAFile       string `mapstructure:"ca_file"`
+	InstanceName string `mapstructure:"instance_name"`
+	Logs         struct {
+		FilePath string `mapstructure:"file_path"`
+	} `mapstructure:"logs"`
+}
+
+func parseDbiPostgresqlInstances(conf *confmap.Conf) []dbiInstanceConfig {
+	arr, _ := conf.Get(dbiPgKey).([]any)
+	var raw []pgRawInstance
+	if err := mapstructure.Decode(arr, &raw); err != nil {
+		return nil
+	}
+	instances := make([]dbiInstanceConfig, 0, len(raw))
+	for _, r := range raw {
+		instances = append(instances, dbiInstanceConfig{
+			endpoint:     r.Endpoint,
+			username:     r.Username,
+			passfile:     r.PasswordFile,
+			caFile:       r.CAFile,
+			instanceName: r.InstanceName,
+			logFilePath:  r.Logs.FilePath,
+			isLocalhost:  isLocalhostEndpoint(r.Endpoint),
+		})
+	}
+	return instances
+}
+
+func isLocalhostEndpoint(endpoint string) bool {
+	return strings.HasPrefix(endpoint, "localhost") ||
+		strings.HasPrefix(endpoint, "127.0.0.1") ||
+		strings.HasPrefix(endpoint, "::1")
+}
