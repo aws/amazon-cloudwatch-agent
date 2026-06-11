24dd5e02830f4ffa9a32dbfb8d351f846f932b09
Mitali Salvi <44349099+mitali-salvi@users.noreply.github.com>
2026-06-11 09:30:13 -0400

    Add prometheus pipeline translator under opentelemetry config (#2148)

---
+++ translator/translate/otel/pipeline/opentelemetry/prometheus/translator.go
@@ -0,0 +1,123 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package prometheus
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/open-telemetry/opentelemetry-collector-contrib/receiver/prometheusreceiver"
+	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/collector/confmap"
+	"go.opentelemetry.io/collector/pipeline"
+	"gopkg.in/yaml.v3"
+
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/common"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/connector/forward"
+	"github.com/aws/amazon-cloudwatch-agent/translator/translate/otel/processor/transformprocessor"
+)
+
+const (
+	pipelineName = "otel_prometheus"
+)
+
+var clusterNameRegex = regexp.MustCompile(`^[a-zA-Z0-9._-]+$`)
+
+var prometheusKey = common.ConfigKey(common.OpenTelemetryKey, common.CollectKey, common.PrometheusKey)
+var configPathKey = common.ConfigKey(prometheusKey, "config_path")
+var clusterNameKey = common.ConfigKey(prometheusKey, "cluster_name")
+
+type translator struct{}
+
+var _ common.PipelineTranslator = (*translator)(nil)
+
+func NewTranslator() common.PipelineTranslator {
+	return &translator{}
+}
+
+func (t *translator) ID() pipeline.ID {
+	return pipeline.NewIDWithName(pipeline.SignalMetrics, pipelineName)
+}
+
+func (t *translator) Translate(conf *confmap.Conf) (*common.ComponentTranslators, error) {
+	if conf == nil || !conf.IsSet(prometheusKey) {
+		return nil, &common.MissingKeyError{ID: t.ID(), JsonKey: prometheusKey}
+	}
+
+	fwdConnector := forward.NewTranslator(common.OpenTelemetryKey)
+	receiver := &prometheusReceiverTranslator{}
+
+	processors := common.NewTranslatorMap[component.Config, component.ID]()
+	if clusterName, ok := common.GetString(conf, clusterNameKey); ok && clusterName != "" {
+		if !clusterNameRegex.MatchString(clusterName) {
+			return nil, fmt.Errorf("cluster_name contains invalid characters: %q", clusterName)
+		}
+		processors.Set(transformprocessor.NewTranslatorWithName("set_cluster_name",
+			transformprocessor.WithMetricStatements([]string{
+				fmt.Sprintf(`set(resource.attributes["k8s.cluster.name"], "%s")`, clusterName),
+			}),
+		))
+	}
+
+	return &common.ComponentTranslators{
+		Receivers:  common.NewTranslatorMap[component.Config, component.ID](receiver),
+		Processors: processors,
+		Exporters:  common.NewTranslatorMap[component.Config, component.ID](fwdConnector),
+		Extensions: common.NewTranslatorMap[component.Config, component.ID](),
+		Connectors: common.NewTranslatorMap[component.Config, component.ID](fwdConnector),
+	}, nil
+}
+
+// prometheusReceiverTranslator reads the prometheus config from config_path.
+type prometheusReceiverTranslator struct{}
+
+var _ common.ComponentTranslator = (*prometheusReceiverTranslator)(nil)
+
+func (t *prometheusReceiverTranslator) ID() component.ID {
+	return component.NewIDWithName(prometheusreceiver.NewFactory().Type(), "")
+}
+
+func (t *prometheusReceiverTranslator) Translate(conf *confmap.Conf) (component.Config, error) {
+	factory := prometheusreceiver.NewFactory()
+	cfg := factory.CreateDefaultConfig().(*prometheusreceiver.Config)
+
+	if !conf.IsSet(configPathKey) {
+		return nil, &common.MissingKeyError{ID: t.ID(), JsonKey: configPathKey}
+	}
+
+	configPath, _ := common.GetString(conf, configPathKey)
+	if configPath == "" {
+		return nil, fmt.Errorf("config_path must not be empty for opentelemetry prometheus receiver")
+	}
+	configPath = filepath.Clean(configPath)
+
+	content, err := os.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read prometheus config from path %s: %w", configPath, err)
+	}
+
+	var stringMap map[string]interface{}
+	if err := yaml.Unmarshal(content, &stringMap); err != nil {
+		return nil, fmt.Errorf("unable to parse prometheus config from %s: %w", configPath, err)
+	}
+
+	componentParser := confmap.NewFromStringMap(stringMap)
+	// NOTE: Prometheus relabel configs use $1, $2 for capture group references.
+	// The OTel confmap expandconverter interprets these as environment variable
+	// references (os.Expand), which can cause failures or empty replacements.
+	// This is a known limitation when prometheus configs with relabel_configs
+	// are loaded through the OTel config resolver pipeline.
+
+	var promCfg prometheusreceiver.PromConfig
+	if err := componentParser.Unmarshal(&promCfg); err != nil {
+		return nil, fmt.Errorf("unable to unmarshal prometheus config from %s: %w", configPath, err)
+	}
+	cfg.PrometheusConfig.GlobalConfig = promCfg.GlobalConfig
+	cfg.PrometheusConfig.ScrapeConfigs = promCfg.ScrapeConfigs
+	cfg.PrometheusConfig.TracingConfig = promCfg.TracingConfig
+
+	return cfg, nil
+}
---
+++ translator/translate/otel/pipeline/opentelemetry/prometheus/translator_test.go
@@ -0,0 +1,221 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT
+
+package prometheus
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/confmap"
+)
+
+func TestPrometheusTranslator(t *testing.T) {
+	tt := NewTranslator()
+	assert.EqualValues(t, "metrics/otel_prometheus", tt.ID().String())
+
+	testCases := map[string]struct {
+		input   map[string]interface{}
+		wantErr bool
+	}{
+		"WithNilConf": {
+			input:   nil,
+			wantErr: true,
+		},
+		"WithoutPrometheusKey": {
+			input:   map[string]interface{}{},
+			wantErr: true,
+		},
+		"WithValidConfig": {
+			input: map[string]interface{}{
+				"opentelemetry": map[string]interface{}{
+					"collect": map[string]interface{}{
+						"prometheus": map[string]interface{}{
+							"config_path": createTempPromConfig(t),
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		"WithClusterName": {
+			input: map[string]interface{}{
+				"opentelemetry": map[string]interface{}{
+					"collect": map[string]interface{}{
+						"prometheus": map[string]interface{}{
+							"config_path":  createTempPromConfig(t),
+							"cluster_name": "my-cluster",
+						},
+					},
+				},
+			},
+			wantErr: false,
+		},
+		"WithInvalidClusterName": {
+			input: map[string]interface{}{
+				"opentelemetry": map[string]interface{}{
+					"collect": map[string]interface{}{
+						"prometheus": map[string]interface{}{
+							"config_path":  createTempPromConfig(t),
+							"cluster_name": `bad"name`,
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+		"WithMissingConfigFile": {
+			input: map[string]interface{}{
+				"opentelemetry": map[string]interface{}{
+					"collect": map[string]interface{}{
+						"prometheus": map[string]interface{}{
+							"config_path": "/nonexistent/path.yml",
+						},
+					},
+				},
+			},
+			wantErr: false, // pipeline translates fine; file error surfaces at receiver Translate time
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			var conf *confmap.Conf
+			if tc.input != nil {
+				conf = confmap.NewFromStringMap(tc.input)
+			}
+			got, err := tt.Translate(conf)
+			if tc.wantErr {
+				require.Error(t, err)
+				assert.Nil(t, got)
+			} else {
+				require.NoError(t, err)
+				assert.NotNil(t, got)
+				assert.Equal(t, 1, got.Receivers.Len())
+				assert.Equal(t, 1, got.Exporters.Len())
+				assert.Equal(t, 1, got.Connectors.Len())
+				assert.Equal(t, "prometheus", got.Receivers.Keys()[0].String())
+				assert.Equal(t, "forward/opentelemetry", got.Exporters.Keys()[0].String())
+			}
+		})
+	}
+}
+
+func TestPrometheusTranslatorClusterNameProcessor(t *testing.T) {
+	conf := confmap.NewFromStringMap(map[string]interface{}{
+		"opentelemetry": map[string]interface{}{
+			"collect": map[string]interface{}{
+				"prometheus": map[string]interface{}{
+					"config_path":  createTempPromConfig(t),
+					"cluster_name": "test-cluster",
+				},
+			},
+		},
+	})
+
+	tt := NewTranslator()
+	got, err := tt.Translate(conf)
+	require.NoError(t, err)
+	assert.Equal(t, 1, got.Processors.Len())
+	assert.Equal(t, "transform/set_cluster_name", got.Processors.Keys()[0].String())
+}
+
+func TestPrometheusTranslatorNoClusterNameProcessor(t *testing.T) {
+	conf := confmap.NewFromStringMap(map[string]interface{}{
+		"opentelemetry": map[string]interface{}{
+			"collect": map[string]interface{}{
+				"prometheus": map[string]interface{}{
+					"config_path": createTempPromConfig(t),
+				},
+			},
+		},
+	})
+
+	tt := NewTranslator()
+	got, err := tt.Translate(conf)
+	require.NoError(t, err)
+	assert.Equal(t, 0, got.Processors.Len())
+}
+
+func TestPrometheusReceiverTranslator(t *testing.T) {
+	configPath := createTempPromConfig(t)
+	conf := confmap.NewFromStringMap(map[string]interface{}{
+		"opentelemetry": map[string]interface{}{
+			"collect": map[string]interface{}{
+				"prometheus": map[string]interface{}{
+					"config_path": configPath,
+				},
+			},
+		},
+	})
+
+	receiver := &prometheusReceiverTranslator{}
+	cfg, err := receiver.Translate(conf)
+	require.NoError(t, err)
+	assert.NotNil(t, cfg)
+}
+
+func TestPrometheusReceiverTranslatorMissingFile(t *testing.T) {
+	conf := confmap.NewFromStringMap(map[string]interface{}{
+		"opentelemetry": map[string]interface{}{
+			"collect": map[string]interface{}{
+				"prometheus": map[string]interface{}{
+					"config_path": "/nonexistent/path.yml",
+				},
+			},
+		},
+	})
+
+	receiver := &prometheusReceiverTranslator{}
+	cfg, err := receiver.Translate(conf)
+	require.Error(t, err)
+	assert.Nil(t, cfg)
+	assert.Contains(t, err.Error(), "unable to read prometheus config")
+}
+
+func createTempPromConfig(t *testing.T) string {
+	t.Helper()
+	content := []byte(`scrape_configs:
+  - job_name: test
+    static_configs:
+      - targets: ['localhost:9090']
+`)
+	dir := t.TempDir()
+	path := filepath.Join(dir, "prometheus.yml")
+	require.NoError(t, os.WriteFile(path, content, 0600))
+	return path
+}
+
+func createTempPlainPromConfig(t *testing.T) string {
+	t.Helper()
+	content := []byte(`global:
+  scrape_interval: 15s
+scrape_configs:
+  - job_name: plain_test
+    static_configs:
+      - targets: ['localhost:9090']
+`)
+	dir := t.TempDir()
+	path := filepath.Join(dir, "prometheus.yml")
+	require.NoError(t, os.WriteFile(path, content, 0600))
+	return path
+}
+
+func TestPrometheusReceiverTranslatorPlainFormat(t *testing.T) {
+	conf := confmap.NewFromStringMap(map[string]interface{}{
+		"opentelemetry": map[string]interface{}{
+			"collect": map[string]interface{}{
+				"prometheus": map[string]interface{}{
+					"config_path": createTempPlainPromConfig(t),
+				},
+			},
+		},
+	})
+
+	receiver := &prometheusReceiverTranslator{}
+	cfg, err := receiver.Translate(conf)
+	require.NoError(t, err)
+	assert.NotNil(t, cfg)
+}
